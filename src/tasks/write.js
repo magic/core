867d485a9ad9ada90596a7f416c9e71ec52d4365
Jascha <jascha@jaeh.at>
2018-12-28 15:01:08 +0100

    cleanup

--- src/tasks/write.js
+++ src/tasks/write.js
@@ -5,13 +5,6 @@ const { parse } = require('@babel/parser')
 const { mkdirp } = require('../lib')
 
 const write = ({ pages, vendor, style, config }) => {
-  write.html({ pages, config })
-  fs.writeFileSync(path.join(config.DIR.TMP, 'vendor.js'), vendor.bundle.code)
-  fs.writeFileSync(path.join(config.DIR.TMP, 'vendor.min.js'), vendor.minified.code)
-  fs.writeFileSync(path.join(config.DIR.TMP, 'main.css'), style.minified)
-}
-
-write.html = ({ pages, config }) => {
   pages.forEach(page => {
     let pagePath = path.join(config.DIR.TMP, page.name)
     mkdirp(pagePath)
@@ -20,6 +13,10 @@ write.html = ({ pages, config }) => {
     }
     fs.writeFileSync(pagePath, page.rendered)
   })
+
+  fs.writeFileSync(path.join(config.DIR.TMP, 'vendor.js'), vendor.bundle.code)
+  fs.writeFileSync(path.join(config.DIR.TMP, 'vendor.min.js'), vendor.minified.code)
+  fs.writeFileSync(path.join(config.DIR.TMP, 'main.css'), style.minified)
 }
 
 module.exports = write
