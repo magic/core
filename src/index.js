d1a3595d1e1e9fb2117acc72b5adb662724a4f65
Jascha <jascha@jaeh.at>
2018-12-28 03:08:44 +0100

    add babel, add minify, add transpile object

--- src/index.js
+++ src/index.js
@@ -6,10 +6,29 @@ const is = require('@magic/types')
 const { renderToString } = require('hyperapp-render')
 const { getFiles, app, mkdirp } = require('./lib/')
 const library = require('./lib/modules')
+const { parse } = require('@babel/parser')
+const generate = require('@babel/generator').default
 
 const config = require('./config')
 
 const isUpperCase = n => n[0].toUpperCase() === n[0]
+
+let presets = [
+  [
+    'env',
+    {
+      modules: false,
+      targets: {
+        browsers: ['last 2 versions', 'safari >= 7'],
+      },
+    },
+  ],
+]
+
+const babelOpts = {
+  sourceMaps: 'both',
+  presets,
+}
 
 const merge = {
   state: (parent, component) => deep.merge(parent.state, component.state),
@@ -61,19 +80,25 @@ const prepare = {
     const componentFile = library.component
     const hyperappFile = path.join(process.cwd(), 'node_modules', 'hyperapp', 'src', 'index.js')
     const hyperappContent = fs.readFileSync(hyperappFile, 'utf8')
-    let vendorString = hyperappContent.replace(/export function (.*)\(/gm, (m, $1) => `window.${$1} = function ${$1}(`)
-    vendorString += `window.C = ${library.component.toString()}\n`
+      .replace(/export function (.*)\(/gm, (_, $1) => `window.${$1} = function ${$1}(`)
+
+
+    let vendorString = ''
+    vendorString += hyperappContent
+
+    vendorString += `const C = window.C = ${library.component.toString()}\n`
       .replace(/attributes/gm, 'a')
       .replace(/name/gm, 'n')
       .replace(/children/gm, 'c')
 
-    vendorString += `['${tags.join(",'")}'].forEach(d => window[d] = C(d);)`
-    components.forEach(comp => {
-      console.log(comp)
+    tags.forEach(tag => {
+      vendorString += `const ${tag} = window.${tag} = C('${tag}')\n`
     })
 
-    vendorString = vendorString
-      .replace(/\n\n*/gim, '\n')
+    components.forEach(comp => {
+      vendorString += `const ${comp} = window.${comp} = ${library[comp].toString()}\n`
+    })
+
     return vendorString
   },
 
@@ -99,6 +124,16 @@ const prepare = {
 
     return deep.flatten(deps)
   },
+  ast: (str, filename = 'testing') => {
+    babelOpts.filename = filename
+    const evald = parse(str, babelOpts)
+    return evald
+  },
+  minify: (ast) => {
+    babelOpts.minified = true
+    const bundle = generate(ast, babelOpts)
+    return bundle
+  }
 }
 
 const isTagUsed = str => fn =>
@@ -134,6 +169,49 @@ const getDependencies = pages => {
   }
 }
 
+const transpile = {
+  vendor: (components, tags) => {
+    const vendor = prepare.vendor({ components, tags })
+    const ast = prepare.ast(vendor)
+    const minified = prepare.minify(ast)
+    fs.writeFileSync(path.join(config.DIR.TMP, 'vendor.js'), vendor)
+    fs.writeFileSync(path.join(config.DIR.TMP, 'vendor.min.js'), minified.code)
+  },
+  pages: pages => {
+    let style = {}
+    pages.forEach(page => {
+      page.dependencies = prepare.dependencies(page.str)
+      page.dependencies.forEach(dep => {
+        const lib = library[dep] || {}
+        if (lib.state) {
+          page.state = deep.merge(lib.state, page.state)
+        }
+        if (lib.actions) {
+          page.actions = deep.merge(lib.actions, page.actions)
+        }
+        if (lib.style) {
+          page.style = deep.merge(lib.style, page.style)
+        }
+      })
+
+      page.rendered = renderToString(app.View(page), page.state, page.actions)
+      style = deep.merge(style, page.style)
+      let pagePath = path.join(config.DIR.TMP, page.name)
+      mkdirp(pagePath)
+      if (!pagePath.endsWith('index.js') && pagePath.endsWith('/')) {
+        pagePath = path.join(pagePath, 'index.html')
+      }
+      fs.writeFileSync(pagePath, page.rendered)
+    })
+
+    return style
+  },
+  style: style => {
+    const preparedStyle = css(style)
+    fs.writeFileSync(path.join(config.DIR.TMP, 'main.css'), preparedStyle.minified)
+  },
+}
+
 const used = {
   selectors: (str, selector, sep = '"', nope = '<') => {
     return str
@@ -155,39 +233,11 @@ const renderApp = () => {
   app.str = app.View.toString()
   app.dependencies = prepare.dependencies(app.str)
 
-  let style = {}
-  pages.forEach(page => {
-    page.dependencies = prepare.dependencies(page.str)
-    page.dependencies.forEach(dep => {
-      const lib = library[dep] || {}
-      if (lib.state) {
-        page.state = deep.merge(lib.state, page.state)
-      }
-      if (lib.actions) {
-        page.actions = deep.merge(lib.actions, page.actions)
-      }
-      if (lib.style) {
-        page.style = deep.merge(lib.style, page.style)
-      }
-    })
-
-    page.rendered = renderToString(app.View(page), page.state, page.actions)
-    style = deep.merge(style, page.style)
-    let pagePath = path.join(config.DIR.TMP, page.name)
-    mkdirp(pagePath)
-    if (!pagePath.endsWith('index.js') && pagePath.endsWith('/')) {
-      pagePath = path.join(pagePath, 'index.html')
-    }
-    fs.writeFileSync(pagePath, page.rendered)
-  })
-
   const { dependencies, components, tags } = getDependencies(pages)
 
-  const vendor = prepare.vendor({ components, tags })
-  console.log(vendor)
-
-  const preparedStyle = css(style)
-  fs.writeFileSync(path.join(config.DIR.TMP, 'main.css'), preparedStyle.minified)
+  const style = transpile.pages(pages)
+  transpile.vendor(components, tags)
+  transpile.style(style)
 }
 
 renderApp()
