10dde8988e71057b7034a270c0ea3833240d4f18
Jascha <jascha@jaeh.at>
2018-12-31 22:39:11 +0100

    add zipped magic.js

--- src/tasks/write.js
+++ src/tasks/write.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const { parse } = require('@babel/parser')
+const zip = require('node-zopfli-es')
 
 const { mkdirp } = require('../lib')
 
@@ -15,8 +16,14 @@ const write = ({ pages, vendor, style, config }) => {
   })
 
   fs.writeFileSync(path.join(config.DIR.TMP, 'magic.js'), vendor.bundle.code)
-  fs.writeFileSync(path.join(config.DIR.TMP, 'vendor.min.js'), vendor.minified.code)
+  const minTempFile = path.join(config.DIR.TMP, 'magic.min.js')
+  fs.writeFileSync(minTempFile, vendor.minified.code)
   fs.writeFileSync(path.join(config.DIR.TMP, 'magic.css'), style.minified)
+
+  if (process.env.NODE_ENV === 'production') {
+    const zipped = zip.gzipSync(vendor.minified.code)
+    fs.writeFileSync(`${minTempFile}.gz`, zipped)
+  }
 }
 
 module.exports = write
