f44e4d4ec24aaa0551a0f608c5a8c0e3c9c2057a
Jascha <jascha@jaeh.at>
2019-01-01 22:32:02 +0100

    format

--- example/assets/Count.js
+++ example/assets/Count.js
@@ -5,13 +5,12 @@ module.exports = {
   actions: {
     count: mod => state => ({ count: state.count + mod }),
   },
-  View: (state, actions) => (
+  View: (state, actions) =>
     div([
       div(`count: ${state.count}`),
       button({ onclick: () => actions.count(1) }, '+1'),
       button({ onclick: () => actions.count(10) }, '+10'),
       button({ onclick: () => actions.count(-1) }, '-1'),
       button({ onclick: () => actions.count(-10) }, '-10'),
-    ])
-  ),
+    ]),
 }
