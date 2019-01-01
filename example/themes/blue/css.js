c148dfc01c8d01616b53d42056ee831f381a58c4
Jascha <jascha@jaeh.at>
2019-01-01 22:32:22 +0100

    make theme a bit fancier

--- example/themes/blue/css.js
+++ example/themes/blue/css.js
@@ -1,7 +1,25 @@
 const theme = {
+  'header.main': {
+    display: 'inline-block',
+    width: '90%',
+
+    '.logo': {
+      float: 'left',
+    },
+    '.Menu': {
+      float: 'right',
+      margin: '2em 0 0',
+
+      li: {
+        float: 'left',
+        margin: '0 0.5em 0 0',
+      },
+    },
+  },
   a: {
     textDecoration: 'none',
     color: 'blue',
+    outline: 'none',
 
     '&:hover': {
       color: 'orange',
