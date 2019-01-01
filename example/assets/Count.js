50f53d89749d57f9977999667b3a5dfc96075641
Jascha <jascha@jaeh.at>
2019-01-01 22:30:23 +0100

    add Count component to assets

---
+++ example/assets/Count.js
@@ -0,0 +1,17 @@
+module.exports = {
+  state: {
+    count: 0,
+  },
+  actions: {
+    count: mod => state => ({ count: state.count + mod }),
+  },
+  View: (state, actions) => (
+    div([
+      div(`count: ${state.count}`),
+      button({ onclick: () => actions.count(1) }, '+1'),
+      button({ onclick: () => actions.count(10) }, '+10'),
+      button({ onclick: () => actions.count(-1) }, '-1'),
+      button({ onclick: () => actions.count(-10) }, '-10'),
+    ])
+  ),
+}
