e5291bee76f211bb5327de92d6078dd4747455a5
Jascha <jascha@jaeh.at>
2019-01-02 21:10:32 +0100

    update watch

--- src/tasks/serve/watch.js
+++ src/tasks/serve/watch.js
@@ -3,31 +3,44 @@ const fs = require('fs')
 const http = require('http')
 const { addTrailingSlash, getContentType, requireNow } = require('../../lib/')
 const is = require('@magic/types')
+const log = require('@magic/log')
 
 const isProd = process.env.NODE_ENV === 'production'
 
 const prepare = require('../prepare')
 const transpile = require('../transpile')
 
-const watch = () => {
-  console.time('watch')
+let watchTime = 100
 
-  console.time('prep')
+const watch = () => {
+  const start = new Date().getTime()
+
   const newApp = requireNow(path.join(process.cwd(), 'src', 'modules', 'app.js'))
   const prep = prepare(newApp)
-  console.timeEnd('prep')
 
-  console.time('transpile')
-  const { pages, css, code }= transpile(prep)
+  const { pages, css, code } = transpile(prep)
   prep.pages = pages
   prep.css = css
   prep.lib.code = code
-  console.timeEnd('transpile')
 
   global.app = prep
 
-  console.timeEnd('watch')
-  setTimeout(() => watch(app), 500)
+  const end = new Date().getTime() - start
+  const hundredths = Math.ceil((end + 99) / 100)
+  let newWatchTime = 0
+  for (let i = 0; i < hundredths; i++) {
+    newWatchTime += 100
+  }
+  if (newWatchTime < 300) {
+    newWatchTime = 300
+  }
+
+  if (newWatchTime !== watchTime) {
+    watchTime = newWatchTime
+    console.log(`setting watch time to ${watchTime}ms`)
+  }
+
+  setTimeout(() => watch(app), watchTime)
 }
 
 module.exports = watch
