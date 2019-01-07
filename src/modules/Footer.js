1b617ad84b20933ca5311e2817946e52b4e58a3a
Jascha <jascha@jaeh.at>
2019-01-07 22:51:11 +0100

    change footer text

--- src/modules/Footer.js
+++ src/modules/Footer.js
@@ -2,7 +2,7 @@ const Footer = {
   View: () =>
     footer({ class: 'main' }, [
       div({ class: 'wrapper' }, [
-        `&copy; ${new Date().getFullYear()} - made with ♥ and `,
+        `© ${new Date().getFullYear()} - made with a few bits of `,
         a({ href: 'https://github.com/magic/core', target: '_blank', rel: 'noopener' }, 'magic'),
       ]),
     ]),
