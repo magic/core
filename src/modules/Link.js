2a4c4c2d962838e340367ec0dccd67e6f99fb7af
Jascha <jascha@jaeh.at>
2019-01-07 22:55:54 +0100

    allow external links

--- src/modules/Link.js
+++ src/modules/Link.js
@@ -1,3 +1,22 @@
 module.exports = {
-  View: ({ to, text }) => (_, actions) => a({ href: to, onclick: actions.go }, text),
+  View: ({ to, text, nofollow, noreferrer }, children) => (_, actions) => {
+    const props = {
+      href: to,
+    }
+
+    if (to.startsWith('/') && !to.startsWith(`//`)) {
+      props.onclick = actions.go
+    } else {
+      props.rel = 'noopener'
+      if (nofollow) {
+        props.rel += ' nofollow'
+      }
+      if (noreferrer) {
+        props.rel += ' noreferrer'
+      }
+      props.target = '_blank'
+    }
+
+    return a(props, text || children)
+  },
 }
