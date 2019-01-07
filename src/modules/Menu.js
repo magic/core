6503e41af3446a901117794f76c47efe58b7fa70
Jascha <jascha@jaeh.at>
2019-01-07 04:39:50 +0100

    Menu.actions.go: return for local #hashlinks

--- src/modules/Menu.js
+++ src/modules/Menu.js
@@ -1,6 +1,9 @@
 const Menu = {
   actions: {
     go: e => state => {
+      if (e.target.href.startsWith('#')) {
+        return
+      }
       e.preventDefault()
       let url = state.url
       if (e.target && e.target.href) {
