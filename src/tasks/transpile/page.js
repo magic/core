543a4437eb36c63dc4c6d43bea63485634a93d82
Jascha <jascha@jaeh.at>
2019-01-07 08:28:29 +0100

    set state.url correctly before ssr

--- src/tasks/transpile/page.js
+++ src/tasks/transpile/page.js
@@ -5,6 +5,10 @@ const { applyWebRoot } = require('../../lib')
 module.exports = app => page => {
   const state = deep.merge(page.state, app.state)
   const actions = deep.merge(page.actions, app.actions)
+
+  // page url must overwrite app url to get .active class assigned correctly on menu items
+  state.url = page.name
+
   const rendered = applyWebRoot(renderToString(app.View(page), state, actions))
 
   return {
