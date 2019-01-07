7bfda9e1dab65f1d6ed10bb2348a2463af841bed
Jascha <jascha@jaeh.at>
2019-01-07 06:06:52 +0100

    export DocHeader from assets

--- example/assets/index.js
+++ example/assets/index.js
@@ -2,5 +2,6 @@ module.exports = {
   Inside: require('./Inside'),
   Wrapper: require('./Wrapper'),
   Count: require('./Count'),
+  DocHeader: require('./DocHeader'),
   Pre: require('@magic-modules/pre'),
 }
