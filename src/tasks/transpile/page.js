88eb8d2a2ad2582d3b9f580dc869d2e973296acc
Jascha <jascha@jaeh.at>
2019-01-07 23:00:18 +0100

    add better error messages

--- src/tasks/transpile/page.js
+++ src/tasks/transpile/page.js
@@ -1,18 +1,26 @@
-const deep = require('@magic/deep')
 const { renderToString } = require('hyperapp-render')
+
+const deep = require('@magic/deep')
+const log = require('@magic/log')
+
 const { applyWebRoot } = require('../../lib')
 
 module.exports = app => page => {
-  const state = deep.merge(page.state, app.state)
-  const actions = deep.merge(page.actions, app.actions)
+  try {
+    const state = deep.merge(page.state, app.state)
+    const actions = deep.merge(page.actions, app.actions)
 
-  // page url must overwrite app url to get .active class assigned correctly on menu items
-  state.url = page.name
+    // page url must overwrite app url to get .active class assigned correctly on menu items
+    state.url = page.name
 
-  const rendered = applyWebRoot(renderToString(app.View(page), state, actions))
+    const rendered = applyWebRoot(renderToString(app.View(page), state, actions))
 
-  return {
-    ...page,
-    rendered,
+    return {
+      ...page,
+      rendered,
+    }
+  } catch(e) {
+    log.error('tasks/transpile/page.js', page.name, e)
+    process.exit(1)
   }
 }
