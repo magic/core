d97117e30789f8a5292a99a97b537574856e7b69
Jascha <jascha@jaeh.at>
2019-01-07 02:47:28 +0100

    check if file exists before reading it

--- src/lib/requireNow.js
+++ src/lib/requireNow.js
@@ -2,11 +2,13 @@ const fs = require('fs')
 const path = require('path')
 
 const requireNow = str => {
-  const file = fs.readFileSync(str, 'utf8')
+  if (fs.existsSync(str)) {
+    const file = fs.readFileSync(str, 'utf8')
 
-  // depending on the file type
-  const e = eval(file)
-  return e
+    // depending on the file type
+    const e = eval(file)
+    return e
+  }
 }
 
 module.exports = requireNow
