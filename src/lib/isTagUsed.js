086fc95e283b5415741a6599ccf36aa3f96c1c70
Jascha <jascha@jaeh.at>
2019-01-07 22:51:00 +0100

    isTagUsed uses regex

--- src/lib/isTagUsed.js
+++ src/lib/isTagUsed.js
@@ -1,13 +1,15 @@
 const isTagUsed = str => fn => {
   const incs = [
-    ` ${fn}(`,
-    `,${fn}(`,
-    `\n${fn}(`,
-    `[${fn}(`,
-    `(${fn})`,
-    `(${fn}(`,
-    `${fn}.View`,
-  ].some(t => str.includes(t))
+    new RegExp(`(\\s+|\\,|\\n|\\[|\\()${fn}(\\(|\\)|\\,|.View)`, 'g'),
+    // `,${fn}(`,
+    // `\n${fn}(`,
+    // `[${fn}(`,
+    // `(${fn})`,
+    // `(${fn}(`,
+    // `${fn}.View`,
+    // ` ${fn},`,
+    // `${fn},`
+  ].some(t => str.match(t))
 
   return incs
 }
