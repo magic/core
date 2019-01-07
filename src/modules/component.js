5228059b104130026696535f591205445f816ca8
Jascha <jascha@jaeh.at>
2019-01-07 22:59:51 +0100

    update is to allow multi checking

--- src/modules/component.js
+++ src/modules/component.js
@@ -1,13 +1,18 @@
 const { h } = require('hyperapp')
 
 const component = name => (attributes = {}, children = false) => {
+  const is = (a, ...t) => t.some(tt => tt === typeof a)
+
   if (!children) {
     if (
-      typeof attributes === 'string' ||
-      typeof attributes === 'number' ||
+      is(attributes, 'string', 'number') ||
       Array.isArray(attributes)
+      || is(attributes, 'function')
     ) {
       children = attributes
+      attributes = {}
+    } else if (is(attributes.View, 'function')) {
+      children = attributes.View
       attributes = {}
     }
   }
