c9d614031a7dc116fc27a19c26a0ca3d51b69a32
Jascha <jascha@jaeh.at>
2019-01-08 22:55:12 +0100

    update footer style and text

--- src/modules/Footer.js
+++ src/modules/Footer.js
@@ -2,16 +2,14 @@ const Footer = {
   style: {
     'footer.main': {
       position: 'relative',
-      bottom: 0,
-      top: 'auto',
       textAlign: 'center',
-      padding: '0 0 .5em',
+      padding: '5em 0 .5em',
     },
   },
   View: () =>
     footer({ class: 'main' }, [
       div({ class: 'wrapper' }, [
-        `© ${new Date().getFullYear()} - made with a few bits of `,
+        `made with a few bits of `,
         Link({ href: 'https://github.com/magic/core', target: '_blank', rel: 'noopener' }, 'magic'),
       ]),
     ]),
