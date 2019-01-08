2cd6b4ca82c21b3a6636970d5dd1ab838c8a9e4d
Jascha <jascha@jaeh.at>
2019-01-08 22:55:27 +0100

    update module docs

--- example/pages/docs/modules.js
+++ example/pages/docs/modules.js
@@ -9,7 +9,63 @@ module.exports = {
 
     h1(state.title),
     p('magic modules are predefined components for webapps.'),
-    h3('list of magic modules'),
+    h3('preinstalled magic modules'),
+    p('magic has some preinstalled modules that will be useful in most pages.'),
+    h4('menu'),
+    p('the Menu module provides... menus.'),
+    p('just pass it a string which is the state key of the menu, add that menu to the /assets/app.js file.'),
+    Pre.View(`
+// assets/app.js
+module.exports = {
+  state: {
+    // ...state
+    menuName: [
+      { to: '/example-page', text: 'example page' },
+      { to: 'https://example.com', text: 'example.com' },
+      { to: 'https://example.com', nofollow: true, noreferrer: true, target: 'utopia', text: 'nofollow and noref" },
+    ],
+  },
+  // ... rest of app.js
+}`),
+
+    p('then, in a page or module'),
+    Pre.View(`
+module.exports = () => Menu('menuName')
+
+// outputs:
+<nav class="Menu">
+  <ul>
+    <li>
+      <a onclick="actions.go" href="{{ WEB_ROOT }}example-page">example page</a>
+    </li>
+    <li>
+      <a href="https://example.com" target="_blank" rel="noopener">example.com</a>
+    </li>
+    <li>
+      <a href="https://example.com" target="utopia" rel="noopener nofollow noreferrer">nofollow and noref</a>
+    </li>
+  </ul>
+</nav>
+}`),
+
+    h3('link'),
+    p('the link allows you to link to things.'),
+    Pre.View(`
+// in any page or component View
+module.exports = () => [
+  Link({ to: '/page', text: 'page' }),
+  // outputs <a href="/page" onclick="actions.go">page</a>
+  Link({ to: 'https://example.com', text: 'page' }),
+  // outputs <a href="https://example.com" target="_blank" rel="noopener">page</a>
+  Link({ to: '/page', text: 'page', nofollow: true, noreferrer: true }),
+  // outputs <a href="https://example.com" target="_blank" rel="nofollow noreferrer noopener">page</a>
+
+  // you can also use children instead of the text prop:
+  Link({ to: '/' }, 'home')
+`),
+
+
+    h3('list of installable magic modules'),
     ul([li([Link({ to: 'https://github.com/magic-modules/pre' }, '@magic-modules/pre')])]),
   ],
 }
