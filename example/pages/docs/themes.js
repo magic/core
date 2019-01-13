40aa2e995317a38b9e2deadda13b0400ee65409d
Jascha <jascha@jaeh.at>
2019-01-13 01:56:38 +0100

    update theme info

--- example/pages/docs/themes.js
+++ example/pages/docs/themes.js
@@ -8,9 +8,24 @@ module.exports = {
     DocHeader,
 
     h1(state.title),
-    p('magic themes are themes for magic apps.'),
+    p(
+      'magic themes are themes for magic apps. you decide which theme to load by specifying the theme name in config.THEME',
+    ),
+    Pre.View(`
+// /config.js
+module.exports = {
+  // ...rest of config,
+  THEME: 'blue',
+}
+`),
+
+    h2('theme load order'),
+    p('themes get loaded from multiple places. last in the list overwrites earlier entries.'),
+    p('/node_modules/@magic/core/src/themes/${config.THEME}/index.js'),
+    p('/node_modules/@magic-themes/${config.THEME}'),
+    p('/assets/themes/${config.THEME}/index.js'),
 
     h2('list of magic themes'),
-    ul([li([Link({ to: 'https://github.com/magic-themes/blue' }, '@magic-themes/blue')])]),
+    ul([li([Link({ to: 'https://github.com/magic-themes' }, 'none yet, coming soon.')])]),
   ],
 }
