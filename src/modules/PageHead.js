3de30d813ad68d10c21cc2205aaa10e1e1cec9e8
Jascha <jascha@jaeh.at>
2019-01-30 00:50:11 +0100

    add link to pagehead

--- src/modules/PageHead.js
+++ src/modules/PageHead.js
@@ -2,7 +2,7 @@ module.exports = state =>
   (state.logo || state.menu || state.tagline) &&
   header({ class: 'main' }, [
     (state.logo || state.logotext) &&
-      div({ class: 'logo-wrapper' }, [
+      Link({ to: '/', class: 'logo-wrapper' }, [
         state.logo && Img({ class: 'logo', src: state.logo }),
         state.logotext && span({ class: 'logo-text' }, state.logotext),
       ]),
