b0f47c9418bb030188717e0809cbfb1ae83831ea
Jascha <jascha@jaeh.at>
2019-01-29 07:37:59 +0100

    keep directory structure when imageminning

--- src/tasks/write/images.js
+++ src/tasks/write/images.js
@@ -5,20 +5,26 @@ const imageminSvgo = require('imagemin-svgo')
 const imageminMozjpeg = require('imagemin-mozjpeg')
 const imageminGifsicle = require('imagemin-gifsicle')
 
-const minifyImages = async images => {
-  const imageGlob = `**/*.{${images.join(',')}}`
-  const input = [path.join(config.DIR.STATIC, imageGlob)]
-  const output = config.DIR.PUBLIC
+const getDirectories = require('../../lib/getDirectories')
 
-  await imagemin(input, output, {
-    plugins: [
-      imageminMozjpeg({ quality: 70 }),
-      imageminPngquant({ quality: [0.6, 0.75] }),
-      imageminGifsicle({ optimizationLevel: 3 }),
-      imageminSvgo({
-        plugins: [{ removeViewBox: false }],
-      }),
-    ],
-  })
+const minifyImages = async images => {
+  const imageGlob = `*.{${images.join(',')}}`
+  const dirs = getDirectories(config.DIR.STATIC)
+
+  await Promise.all(dirs.map(async dir => {
+    const input = [path.join(dir, imageGlob)]
+    const output = dir.replace(config.DIR.STATIC, config.DIR.PUBLIC)
+
+    return await imagemin(input, output, {
+      plugins: [
+        imageminMozjpeg({ quality: 90 }),
+        imageminPngquant({ quality: [0.85, 0.9] }),
+        imageminGifsicle({ optimizationLevel: 3 }),
+        imageminSvgo({
+          plugins: [{ removeViewBox: false }],
+        }),
+      ],
+    })
+  }))
 }
 module.exports = minifyImages
