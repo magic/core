c48e0f5ede337a73dc5da48d37f54e0b9bca5796
Jascha <jascha@jaeh.at>
2019-03-10 00:07:27 +0100

    format

--- src/tasks/serve/index.js
+++ src/tasks/serve/index.js
@@ -2,9 +2,13 @@ const http = require('http')
 const handler = require('./handler')
 
 const serve = app => {
-  http.createServer(handler(app)).listen(3000, () => {
+  const server = http.createServer(handler(app))
+
+  server.listen(3000, () => {
     console.log('listening to http://localhost:3000')
   })
+
+  return server
 }
 
 module.exports = serve
