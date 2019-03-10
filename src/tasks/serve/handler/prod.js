fc3e6ea75ee6d8c9d6f21bc9b3fdc212859d668b
Jascha <jascha@jaeh.at>
2019-03-10 03:23:09 +0100

    handle lambdas

--- src/tasks/serve/handler/prod.js
+++ src/tasks/serve/handler/prod.js
@@ -25,29 +25,45 @@ const handler = async app => {
       static[rootName] = await fs.readFile(name)
     }))
   }
+  const hasStatic = Object.keys(static).length > 0
 
   const lambdas = {}
   if (apiFiles.length) {
     apiFiles.forEach(name => {
-      const rootName = name.replace(config.DIR.API, '')
+      const rootName = name.replace(config.DIR.API, '').replace('.js', '/')
       lambdas[rootName] = require(name)
     })
   }
+  const hasApi = Object.keys(static).length > 0
 
   return async (req, res) => {
     const url = parse(req.url.replace(config.WEB_ROOT, '/'))
+    const { pathname } = url
 
-    const tryStaticFile = url.pathname
-    if (static[tryStaticFile]) {
-      res.writeHead(200, getContentType(tryStaticFile))
-      res.end(static[tryStaticFile])
+    if (config.FOR_DEATH_CAN_NOT_HAVE_HIM) {
+      res.setHeader('X-Clacks-Overhead', 'GNU Terry Pratchet')
     }
 
-    const tryIndexHtml = `${tryStaticFile}index.html`
-    if (static[tryIndexHtml]) {
-      res.writeHead(200, { 'Content-Type': 'text/html' })
-      res.end(static[tryIndexHtml])
-      return
+    if (hasStatic) {
+      const content = static[pathname] || static[`${pathname}index.html`]
+      if (content) {
+        let contentType = getContentType(pathname)
+        if (!static[pathname]) {
+          contentType = 'text/html'
+        }
+        res.writeHead(200, contentType)
+        res.end(content)
+        return
+      }
+    }
+
+    if (req.method === 'POST') {
+      if (hasApi && pathname.startsWith('/api')) {
+        const action = pathname.replace('/api', '')
+        if (typeof lambdas[action] === 'function') {
+          return lambdas[action](req, res)
+        }
+      }
     }
 
     res.writeHead(404, { 'Content-Type': 'text/html' })
