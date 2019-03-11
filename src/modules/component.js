399883cf722c74acf8a3c2c0e8419ef2cb3f85f9
Jascha <jascha@jaeh.at>
2019-03-12 00:08:41 +0100

    format

--- src/modules/component.js
+++ src/modules/component.js
@@ -4,10 +4,7 @@ const component = name => (props = {}, children = false) => {
   const is = (ele, ...types) => types.some(type => type === typeof ele)
 
   if (!children) {
-    if (
-      is(props, 'string', 'number', 'function') ||
-      Array.isArray(props)
-    ) {
+    if (is(props, 'string', 'number', 'function') || Array.isArray(props)) {
       children = props
       props = {}
     } else if (is(props.View, 'function')) {
