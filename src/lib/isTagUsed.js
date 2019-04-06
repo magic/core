a833882abe8f1d26b1509bec6e153b55c713fc68
Jascha <jascha@jaeh.at>
2019-04-06 22:15:36 +0200

    update isTagUsed to correctly detect module.UpperCaseNames, add comments

--- src/lib/isTagUsed.js
+++ src/lib/isTagUsed.js
@@ -1,6 +1,10 @@
 const isTagUsed = str => fn => {
+  // this function will search str for different kinds of module usage.
+  // before the string it will look for whitespace, comma, newline and function call ends ()
+  // after the string it will look for opening brackets, closing brackets, comma, .UpperCasedNames
+
   const incs = [
-    new RegExp(`(\\s+|\\,|\\n|\\[|\\()${fn}(\\(|\\)|\\,|.View)`, 'g'),
+    new RegExp(`(\\s+|\\,|\\n|\\[|\\()${fn}(\\(|\\)|\\,|.[A-Z][a-zA-Z0-9]*)`, 'g'),
     // `,${fn}(`,
     // `\n${fn}(`,
     // `[${fn}(`,
