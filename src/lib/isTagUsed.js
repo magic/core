6b3e7103d91fc3719e97050ef7dc471670e48819
Jascha <jascha@jaeh.at>
2019-04-09 03:46:10 +0200

    refactor isTagUsed to never error

--- src/lib/isTagUsed.js
+++ src/lib/isTagUsed.js
@@ -1,18 +1,21 @@
-const isTagUsed = str => fn => {
-  // this function will search str for different kinds of module usage.
-  // before the string it will look for whitespace, comma, newline and function call ends ()
-  // after the string it will look for opening brackets, closing brackets, comma, .UpperCasedNames
+// this function will search str for different kinds of module usage.
+// before the string it will look for whitespace, comma, newline and function call ends ()
+// after the string it will look for opening brackets, closing brackets, comma, .UpperCasedNames
 
+const isTagUsed = str => fn => {
   // remove text in strings
   str = str
     .replace(/".*"/, '')
     .replace(/'.*'/, '')
     .replace(/`.*`/, '')
 
-  const tag = new RegExp(`(\\s+|\\,|\\n|\\[|\\()${fn}(\\(|\\)|\\,|\.[A-Z][a-zA-Z0-9]*)`, 'gm')
+  const beforeFn = '(\\s+|\\,|\\n|\\[|\\()'
+  const afterFn = '(\\(|\\)|\\,|\.[A-Z][a-zA-Z0-9]*)'
+
+  const tag = new RegExp(`${beforeFn}${fn}${afterFn}`, 'gm')
   const includes = str.match(tag)
 
-  return includes
+  return includes === null ? false : includes.length > 0
 }
 
 module.exports = isTagUsed
