f20f8fcf5021b6e8119dc5efb913b4de1e079645
Jascha <jascha@jaeh.at>
2019-04-12 00:06:05 +0200

    add Mod to docs

--- example/pages/modules.js
+++ example/pages/modules.js
@@ -49,7 +49,68 @@ module.exports = () => div([
     h2('Mod.View and Mod.Component:'),
 
     Mod.View,
+
     Mod.Component,
+
+    h3('Mod sourcecode:'),
+
+    Pre.View(`const Mod = {
+  View: state =>
+    div({ class: 'Mod View' }, [
+      h3('Mod.View'),
+      p([
+        'this is Mod.View. it gets loaded from ',
+        Link({ to: 'https://github.com/magic/core/example/assets/module.js' }, '/assets/module.js'),
+      ]),
+      p([
+        'and imported in ',
+        Link({ to: 'https://github.com/magic/core/example/assets/index.js' }, '/assets/index.js'),
+      ]),
+      p(['the state of this module: ', JSON.stringify(state.module)]),
+    ]),
+
+  Component: () =>
+    div({ class: 'Mod Component' }, [
+      h3('Mod.Component'),
+      p([
+        'Mod.Component, a second component in ',
+        Link({ to: 'https://github.com/magic/core/example/assets/module.js' }, '/assets/module.js'),
+      ]),
+    ]),
+
+  state: {
+    module: {
+      test: 'testing',
+    },
+  },
+
+  style: {
+    '.Mod': {
+      margin: '0 0 1em',
+      padding: '0.5em',
+      border: '1px solid',
+
+      h3: {
+        margin: 0,
+      },
+
+      '&.View': {
+        borderColor: 'green',
+      },
+      '&.Component': {
+        borderColor: 'red',
+      },
+    },
+  },
+
+  global: {
+    state: {
+      module: true,
+    },
+  },
+}
+
+module.exports = Mod`),
 
     h2('preinstalled magic modules'),
     p('magic has some preinstalled modules that will be used in most pages.'),
