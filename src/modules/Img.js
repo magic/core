d7ebfb005406b9142c35c069d6b4e782cca54562
Jascha <jascha@jaeh.at>
2019-04-14 19:39:48 +0200

    Img: accept props.title, accept string as props

--- src/modules/Img.js
+++ src/modules/Img.js
@@ -1,11 +1,21 @@
 module.exports = props => () => {
+  if (typeof props === 'string') {
+    props = {
+      src: props,
+    }
+  }
+
   if (!props.src) {
     return
   }
 
   if (!props.alt) {
-    props.role = 'presentation'
-    props.alt = ''
+    if (props.title) {
+      props.alt = props.title
+    } else {
+      props.role = 'presentation'
+      props.alt = ''
+    }
   }
 
   return img(props)
