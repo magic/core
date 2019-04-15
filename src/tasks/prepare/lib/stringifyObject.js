25d470139d3004ee370cd46d185247c3afc65ef7
Jascha <jascha@jaeh.at>
2019-04-15 18:51:16 +0200

    prepare.stringifyObject: use ' instead of " for object keys and values

--- src/tasks/prepare/lib/stringifyObject.js
+++ src/tasks/prepare/lib/stringifyObject.js
@@ -4,14 +4,14 @@ const stringifyObject = (obj, indent = '') => {
   indent = indent + '  '
 
   if (is.string(obj)) {
-    obj = `"${obj}"`
+    obj = `'${obj}'`
   } else if (is.array(obj)) {
     obj = `[${obj.map(o => stringifyObject(o, indent)).join(',')}]`
   } else if (is.fn(obj)) {
     obj = obj.toString()
   } else if (is.obj(obj)) {
     let str = Object.entries(obj)
-      .map(([k, o]) => `${indent}"${k}": ${stringifyObject(o, indent)}`)
+      .map(([k, o]) => `${indent}'${k}': ${stringifyObject(o, indent)}`)
       .join(',\n')
 
     obj = `${indent}{\n${str}\n${indent}}\n`
