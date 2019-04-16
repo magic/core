5f309fcd14654976f181c96c2a2f021a1027fa2a
Jascha <jascha@jaeh.at>
2019-04-16 19:59:18 +0200

    better help text, add start task

--- src/bin.js
+++ src/bin.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const is = require('@magic/types')
+
 const hasArgv = () =>
   process.argv.indexOf('-h') > -1 ||
   process.argv.indexOf('--h') > -1 ||
@@ -7,7 +9,20 @@ const hasArgv = () =>
   process.argv.indexOf('--help') > -1 ||
   process.argv.indexOf('help') > -1
 
-if (hasArgv()) {
+const tasks = ['clean', 'connect', 'publish', 'build', 'serve']
+const cmds = {}
+tasks
+  .filter(task => process.argv.includes(task))
+  .forEach(task => {
+    cmds[task] = true
+  })
+
+const devAliases = ['dev', 'development', 'start']
+if (devAliases.some(arg => process.argv.includes(arg))) {
+  cmds.serve = true
+}
+
+if (hasArgv() || is.empty(cmds)) {
   console.log(`
 magic - static and serverless page generator
 
@@ -19,8 +34,8 @@ NODE_ENV=production magic [...TASKS]
 
 available tasks:
 start   - starts magic dev env, same as npm run dev
-dev     - run BUILD and SERVE
-build   - builds the bundle.
+dev     - prepare and transpile, then serve app from memory
+build   - builds the client app
 serve   - build and serve the bundle
 clean   - delete public dir
 connect - connect to github (only needs to be done once)
@@ -29,18 +44,6 @@ publish - publish to github
 help    - this help text
 `)
   process.exit()
-}
-
-const tasks = ['clean', 'connect', 'publish', 'build', 'serve']
-const cmds = {}
-tasks
-  .filter(task => process.argv.includes(task))
-  .forEach(task => {
-    cmds[task] = true
-  })
-
-if (process.argv.includes('dev') || process.argv.includes('development')) {
-  cmds.serve = true
 }
 
 const run = require('./cluster.js')
