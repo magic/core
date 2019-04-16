4a3d2334685689692f27a6e1cdcbacd6e3711ec8
Jascha <jascha@jaeh.at>
2019-04-16 20:28:27 +0200

    remove duplicates from getDirectories

--- src/lib/getDirectories.js
+++ src/lib/getDirectories.js
@@ -5,24 +5,20 @@ const deep = require('@magic/deep')
 // recursively find all directories in a directory.
 // returns array of paths relative to dir
 
-const getFilePath = dir => async file => {
+const getFilePath = async (dir, file) => {
   const filePath = path.join(dir, file)
 
   const stat = await fs.stat(filePath)
   if (stat.isDirectory(filePath)) {
-    return [
-      filePath,
-      await getDirectories(filePath),
-    ]
+    return await getDirectories(filePath)
   }
 }
 
 const getDirectories = async dir => {
   const dirContent = await fs.readdir(dir)
-  const dirs = await Promise.all(dirContent.map(getFilePath(dir)))
-
-  const flattened = deep.flatten(dirs)
-  return flattened.filter(a => a)
+  const dirs = await Promise.all(dirContent.map(async file => await getFilePath(dir, file)))
+  const flattened = deep.flatten(dir, dirs).filter(a => a)
+  return flattened
 }
 
 module.exports = getDirectories
