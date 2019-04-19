0e30185ccd0e7f194f9920a211d587bd0b385bf8
Jascha <jascha@jaeh.at>
2019-04-19 23:23:26 +0200

    transpile/page: assign state.url in state declaration

--- src/tasks/transpile/page.js
+++ src/tasks/transpile/page.js
@@ -5,11 +5,8 @@ const deep = require('@magic/deep')
 const { applyWebRoot } = require('../../lib')
 
 module.exports = app => page => {
-  const state = deep.merge(page.state, app.state)
+  const state = deep.merge(page.state, app.state, { url: page.name })
   const actions = deep.merge(page.actions, app.actions)
-
-  // page url must overwrite app url to get .active class assigned correctly on menu items
-  state.url = page.name
 
   const rendered = applyWebRoot(config, renderToString(app.View(page), state, actions))
 
