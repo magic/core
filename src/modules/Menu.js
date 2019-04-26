f0c442269d905f404c85b56d9753a8c151da89db
Jascha <jascha@jaeh.at>
2019-04-26 08:21:20 +0200

    Menu rebuild

--- src/modules/Menu.js
+++ src/modules/Menu.js
@@ -1,82 +1,93 @@
-const Menu = {
-  style: {
-    '.Menu': {
-      float: 'right',
-      margin: '1.5em 0 0',
-      position: 'relative',
+const style = {
+  float: 'right',
+  margin: '1.5em 0 0',
+  position: 'relative',
 
-      li: {
-        float: 'left',
-        margin: '0 .5em 0 0',
-      },
+  li: {
+    float: 'left',
+    margin: '0 .5em 0 0',
 
-      '&.right': {
-        float: 'right',
-      },
-
-      '&.block': {
-        li: {
-          clear: 'both',
-          margin: '0.5em 0 0',
-        },
-      },
-
-      '.active': {
-        '> a': {
-          textDecoration: 'underline',
-        },
-      },
-
-      ul: {
-        ul: {
-          position: 'absolute',
-          left: 0,
-        },
+    '&.active': {
+      '> a': {
+        textDecoration: 'underline',
       },
     },
   },
 
-  View: ({ name = 'menu', class: cl = '', between = false, items = [] }) => state => {
-    if (!items.length && (!state[name] || !state[name].length)) {
-      return
-    }
+  '&.right': {
+    float: 'right',
+  },
 
-    if (!items.length) {
-      items = state[name]
-    }
+  '&.block': {
+    li: {
+      clear: 'both',
+      margin: '0.5em 0 0',
+    },
+  },
 
-    return nav({ class: `Menu ${cl}` }, [
-      ul(
-        items.map((menuItem, i) => {
-          const { items, ...item } = menuItem
-          const props = {}
-          const hash = state.hash ? `#${state.hash}` : ''
-          const url = state.url + hash
-          if (item.to === url) {
-            props.class = 'active'
-          }
-
-          let children
-          if (items && url.startsWith(item.to)) {
-            children = ul(
-              items.map(itm => {
-                const p = {}
-                if (itm.to === url) {
-                  p.class = 'active'
-                }
-                return li(p, Link(itm))
-              }),
-            )
-          }
-
-          return [
-            li(props, [Link(item), children]),
-            between && i < state[name].length - 1 ? li(between) : '',
-          ]
-        }),
-      ),
-    ])
+  ul: {
+    ul: {
+      position: 'absolute',
+      left: 0,
+    },
   },
 }
 
-module.exports = Menu
+const View = (props = 'menu') => state => {
+  if (typeof props === 'string') {
+    props = { name: props }
+  }
+
+  const { name = 'menu', between = false } = props
+  let { class: cl = 'Menu', items = [] } = props
+  items = !items.length && state[name] ? state[name] : items
+
+  const hash = state.hash ? `#${state.hash}` : ''
+  const url = state.url + hash
+
+  if (!cl) {
+    cl = 'Menu'
+  } else if (!cl.includes('Menu')) {
+    cl = `Menu ${cl}`
+  }
+
+  if (!items.length) {
+    return
+  }
+
+  return nav(
+    { class: cl },
+    ul(
+      items.map(({ items = [], ...item }, i) => {
+        // if the item has no values, we quit
+        if (!item.to && !item.text) {
+          return
+        }
+        const props = {}
+        if (item.to === url) {
+          props.class = 'active'
+        }
+
+        let children
+        if (items && url.startsWith(item.to)) {
+          children = ul(
+            items.map(itm => {
+              const p = {}
+              if (itm.to === url) {
+                p.class += 'active'
+              }
+              return li(p, Link(itm))
+            }),
+          )
+        }
+
+        return [li(props, [item.to ? Link(item) : span(item.text), children]), between && i < items.length - 1 ? li(between) : '']
+      }),
+    ),
+  )
+}
+
+module.exports = {
+  style,
+  View,
+}
