dd6963e5e3a81bdcd1db2786d7c562a71870bba2
Jascha <jascha@jaeh.at>
2019-04-26 20:47:56 +0200

    add concepts submenu

--- example/app.js
+++ example/app.js
@@ -5,7 +5,60 @@ module.exports = {
       description: 'Custom App Description',
     },
     menu: [
-      { to: '/concepts/', text: 'concepts' },
+      {
+        to: '/concepts/',
+        text: 'concepts',
+        items: [
+          { to: '/concepts/#modules', text: 'modules' },
+          {
+            to: '/concepts/#state',
+            text: 'state',
+            items: [{ to: '/concepts/#state-example', text: 'example state' }],
+          },
+          {
+            to: '/concepts/#actions',
+            text: 'actions',
+            items: [{ to: '/concepts/#actions-example', text: 'example actions' }],
+          },
+          {
+            to: '/concepts/#views',
+            text: 'views',
+            items: [{ to: '/concepts/#views-example', text: 'example view' }],
+          },
+          {
+            to: '/concepts/#styles',
+            text: 'styles',
+            items: [
+              { to: '/concepts/#styles-magic-css', text: '@magic/css' },
+              { to: '/concepts/#styles-example', text: 'example styles' },
+            ],
+          },
+          {
+            to: '/concepts/#globals',
+            text: 'global',
+            items: [{ to: '/concepts/#globals-example', text: 'example global' }],
+          },
+          {
+            to: '/concepts/#lambdas',
+            text: 'server lambdas',
+            items: [
+              { to: '/concepts/#lambdas-example', text: 'single lambda' },
+              { to: '/concepts/#lambdas-example-multi', text: 'multiple lambdas' },
+            ],
+          },
+          {
+            to: '/concepts/#libs',
+            text: 'external libs',
+            items: [
+              { to: '/concepts/#libs-dir-or-file', text: 'lib dir or file' },
+              { to: '/concepts/#libs-example-file', text: 'example lib file' },
+              { to: '/concepts/#libs-app', text: 'app.lib' },
+              { to: '/concepts/#libs-module', text: 'module.lib' },
+            ],
+          },
+          { to: '/concepts/#full-example', text: 'full example' },
+        ],
+      },
       {
         to: '/files/',
         text: 'files & directories',
