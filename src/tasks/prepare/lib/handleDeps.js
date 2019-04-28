e134b53f64603a0cffa76fbe293998a26f2b0b9f
Jascha <jascha@jaeh.at>
2019-04-28 16:49:13 +0200

    add .View prop to functional components

--- src/tasks/prepare/lib/handleDeps.js
+++ src/tasks/prepare/lib/handleDeps.js
@@ -2,32 +2,38 @@ const is = require('@magic/types')
 const { isUpperCase } = require('../../../lib/')
 
 const handleDependencies = ([name, component]) => {
-  if (is.fn(component)) {
-    if (global.tags.body[name]) {
-      return `const ${name} = C('${name}')\n`
-    }
-
-    return `const ${name} = ${component.toString()}\n`
+  if (is.fn(component) && global.tags.body[name]) {
+    return `const ${name} = C('${name}')\n`
   }
 
   const views = Object.entries(component)
     .filter(([name]) => isUpperCase(name))
     .map(([name, view]) => ({
       name,
-      view,
+      view: view.toString(),
     }))
 
-  if (views.length) {
-    component = `const ${name} = {`
-    views.forEach(view => {
-      component += `  ${view.name}: ${view.view},\n`
-    })
-    component += '}\n'
+  let comp = `const ${name} = `
+  if (is.fn(component)) {
+    comp += `${component.toString()}\n`
   } else {
-    throw new Error(`unknown dependency type in ${name}: ${typeof component}`)
+    comp += ' {\n'
   }
 
-  return component
+  if (views.length) {
+    views.forEach(view => {
+      if (is.fn(component)) {
+        comp += `${name}.${view.name} = ${view.view}\n`
+      } else {
+        comp += `  ${view.name}: ${view.view},\n`
+      }
+    })
+    if (!is.fn(component)) {
+      comp += '}'
+    }
+  }
+
+  return `${comp}\n`
 }
 
 module.exports = handleDependencies
