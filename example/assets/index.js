b1d379427365d5651d14767264b0844e7f75bbe9
Jascha <jascha@jaeh.at>
2019-05-03 21:27:14 +0200

    update docs

--- example/assets/index.js
+++ example/assets/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   Mod: require('./Mod'),
   Pre: require('@magic-modules/pre'),
-  GitBadges: require('@magic-modules/gitbadges'),
+  GitBadges: require('@magic-modules/git-badges'),
   ModuleList: require('@magic-modules/module-list'),
   ThemeList: require('@magic-modules/theme-list'),
   ClientList: require('@magic-modules/client-list'),
