e485a1dcd41e5a485d7757065c952aff361d3e5a
Jascha <jascha@jaeh.at>
2019-05-06 01:12:04 +0200

    remove unneeded divs

--- example/pages/files.js
+++ example/pages/files.js
@@ -80,84 +80,68 @@ module.exports = {
         li('/assets/Menu.js - custom Menu for your app'),
       ]),
 
-      div([
-        h2({ id: 'pages' }, '/pages'),
-        p('the pages dir contains the pages of your webapp.'),
-        p([
-          "each page has it's own state and actions, ",
-          "but also inherits the global state and actions from the app and it's dependencies",
-        ]),
-
-        h3({ id: 'pages-dir-structure' }, 'pages directory to url map, for the domain mag.ic:'),
-        ul([
-          li('/pages/index.js === http://mag.ic/'),
-          li('/pages/pageName.js === http://mag.ic/pageName/'),
-          li('/pages/page-name.js === http://mag.ic/page-name/'),
-          li('/pages/page_name.js === http://mag.ic/page_name/'),
-          li('/pages/dir/index.js === http://mag.ic/dir/'),
-          li('/pages/dir/name.js === http://mag.ic/dir/name/'),
-        ]),
-
-        h3({ id: 'pages-example' }, 'example page:'),
-        Pre(examples.page),
+      h2({ id: 'pages' }, '/pages'),
+      p('the pages dir contains the pages of your webapp.'),
+      p([
+        "each page has it's own state and actions, ",
+        "but also inherits the global state and actions from the app and it's dependencies",
       ]),
 
-      div([
-        h2({ id: 'assets' }, '/assets'),
-        p('the assets dir contains custom components of your app.'),
-        p('you can additionally import @magic-modules here'),
-        h3({ id: 'assets-example' }, 'example /assets/index.js'),
-        Pre(examples.assets),
+      h3({ id: 'pages-dir-structure' }, 'pages directory to url map, for the domain mag.ic:'),
+      ul([
+        li('/pages/index.js === http://mag.ic/'),
+        li('/pages/pageName.js === http://mag.ic/pageName/'),
+        li('/pages/page-name.js === http://mag.ic/page-name/'),
+        li('/pages/page_name.js === http://mag.ic/page_name/'),
+        li('/pages/dir/index.js === http://mag.ic/dir/'),
+        li('/pages/dir/name.js === http://mag.ic/dir/name/'),
       ]),
 
-      div([
-        h2({ id: 'static' }, '/assets/static'),
-        p('the static dir contains all of your static assets.'),
-        p('every file in this directory gets copied to the public dir.'),
-        p('image and svg files get minified using imagemin'),
-        p([
-          'text and binary files get compressed using the optional ',
-          Link({ to: 'https://github.com/jaeh/node-zopfli-es' }, 'node-zopfli-es'),
-          ' (if it is installed)',
-        ]),
+      h3({ id: 'pages-example' }, 'example page:'),
+      Pre(examples.page),
+
+      h2({ id: 'assets' }, '/assets'),
+      p('the assets dir contains custom components of your app.'),
+      p('you can additionally import @magic-modules here'),
+      h3({ id: 'assets-example' }, 'example /assets/index.js'),
+      Pre(examples.assets),
+
+      h2({ id: 'static' }, '/assets/static'),
+      p('the static dir contains all of your static assets.'),
+      p('every file in this directory gets copied to the public dir.'),
+      p('image and svg files get minified using imagemin'),
+      p([
+        'text and binary files get compressed using the optional ',
+        Link({ to: 'https://github.com/jaeh/node-zopfli-es' }, 'node-zopfli-es'),
+        ' (if it is installed)',
       ]),
 
-      div([
-        h2({ id: 'themes' }, '/assets/themes'),
-        p('the themes directory contains... themes.'),
-        p(
-          'at the moment this is file based, which means you have to manually import themes there.',
-        ),
+      h2({ id: 'themes' }, '/assets/themes'),
+      p('the themes directory contains... themes.'),
+      p('at the moment this is file based, which means you have to manually import themes there.'),
 
-        h3({ id: 'themes-example' }, 'example theme'),
-        Pre(examples.theme),
-      ]),
+      h3({ id: 'themes-example' }, 'example theme'),
+      Pre(examples.theme),
 
-      div([
-        h2({ id: 'app' }, '/assets/app.js'),
-        p('the /app.js file allows you to set global state, actions, and styles'),
+      h2({ id: 'app' }, '/assets/app.js'),
+      p('the /app.js file allows you to set global state, actions, and styles'),
 
-        h3({ id: 'app-example' }, 'example /app.js'),
-        Pre(examples.app),
-      ]),
+      h3({ id: 'app-example' }, 'example /app.js'),
+      Pre(examples.app),
 
-      div([
-        h2({ id: 'config' }, '/config.js'),
-        p('the /config.js file allows you to set various aspects of your app'),
+      h2({ id: 'config' }, '/config.js'),
+      p('the /config.js file allows you to set various aspects of your app'),
 
-        h3({ id: 'config-example' }, 'example /config.js'),
-        Pre(examples.config),
-      ]),
+      h3({ id: 'config-example' }, 'example /config.js'),
+      Pre(examples.config),
 
-      div([
-        h2({ id: 'menu' }, '/assets/Menu.js'),
-        p('the /assets/Menu.js file allows you to replace the default Menu component'),
-        h3({ id: 'menu-example' }, 'example /assets/Menu.js'),
-        Link(
-          { to: 'https://github.com/magic/core/blob/master/src/modules/Menu.js' },
-          'Menu.js on github',
-        ),
-      ]),
+      h2({ id: 'menu' }, '/assets/Menu.js'),
+      p('the /assets/Menu.js file allows you to replace the default Menu component'),
+      h3({ id: 'menu-example' }, 'example /assets/Menu.js'),
+      Link(
+        { to: 'https://github.com/magic/core/blob/master/src/modules/Menu.js' },
+        'Menu.js on github',
+      ),
     ]
   },
 }
