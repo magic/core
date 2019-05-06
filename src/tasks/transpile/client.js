5bbcedb90046014ff7f2e7246e2bc7aab6e207dc
Jascha <jascha@jaeh.at>
2019-05-06 16:48:06 +0200

    begin adding babel traverse

--- src/tasks/transpile/client.js
+++ src/tasks/transpile/client.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const fs = require('fs')
 const browserify = require('browserify')
+const babel = require('@babel/core')
+
 
 const isProd = config.ENV === 'production'
 const isDev = !isProd
@@ -25,12 +27,13 @@ const plugins = [
 ]
 
 if (isProd) {
-  if (!process.argv.includes('--no-mangle-names')) {
+  const minify = !process.argv.includes('--no-minify')
+  if (minify && !process.argv.includes('--no-mangle-names')) {
     plugins.push(['minify-mangle-names', { topLevel: true }])
   }
 
   if (!process.argv.includes('--keep-dead-code')) {
-    plugins.push('minify-dead-code-elimination')
+    plugins.push(['minify-dead-code-elimination'])
   }
 
   if (!process.argv.includes('--keep-console')) {
@@ -48,13 +51,16 @@ if (isProd) {
     },
   ])
 
-  if (!process.argv.includes('--no-minify')) {
+  if (minify) {
     plugins.push('minify-simplify')
-    plugins.push('minify-type-constructors')
+    // seems to have no real benefit
+    // plugins.push('minify-type-constructors')
     plugins.push('minify-builtins')
     plugins.push('transform-minify-booleans')
   }
 }
+
+// console.log({ plugins })
 
 const babelOpts = {
   filename: 'magic.js',
@@ -66,9 +72,54 @@ const babelOpts = {
   plugins,
 }
 
-module.exports = ({ str }) => {
+module.exports = async ({ str }) => {
   const filePath = path.join(process.cwd(), '.__browserify_empty.js')
   fs.writeFileSync(filePath, str)
+
+  // const ast = await babel.parseAsync(str)
+
+  // const isLink = val => !val.includes(' ') && (val.includes('://') || val.startsWith('/')) && val !== '://' && val.length > 1
+
+  // babel.traverse(ast, {
+  //   StringLiteral(path) {
+  //     if (isLink(path.node.value)) {
+  //       console.log(path)
+  //       if (path.container.type === 'ObjectProperty') {
+
+  //       }
+  //       process.exit(1)
+  //     }
+  //   },
+  //   // ObjectProperty(path) {
+  //   //   const paths = ['to', 'src', 'href', 'to', 'action', 'logo']
+  //   //   if (paths.includes(path.node.key.name)) {
+  //   //     if (node.type === 'TemplateLiteral') {
+
+  //   //     } else if (node.type === '')
+  //   //     console.log(path.node.value)
+  //   //     // process.exit(1)
+  //   //   }
+  //   // },
+  //   enter(path) {
+  //     if (path.node.name) {
+  //       // if (path.node.name === 'Link') {
+  //       //   const { hub, ...rest } = path
+  //       //   console.log('is link', rest)
+
+  //       //   process.exit(1)
+  //       // }
+  //       // if (global[path.node.name]) {
+  //       //   console.log(path.node.name)
+  //       // }
+  //     }
+  //     // if (path.isIdentifier({ name: "n" })) {
+  //     //   path.node.name = "x";
+  //     // }
+  //   }
+  // })
+
+  // const res = await babel.transformFromAstAsync(ast, str, babelOpts)
+  // console.log({res})
 
   return new Promise((res, rej) =>
     browserify(filePath)
