b7e5bdb8630f700ef43ad101e988fe44776b354b
Jascha <jascha@jaeh.at>
2019-05-08 22:29:36 +0200

    update footer usage

--- example/pages/modules.js
+++ example/pages/modules.js
@@ -275,28 +275,23 @@ module.exports = () => [
     ),
     Pre(`
 // /assets/Footer.js:
-const Footer = {
-  style: {
-    'footer.main': {
-      position: 'relative',
-      textAlign: 'center',
-      padding: '5em 0 .5em',
-    },
+const Footer = () =>
+footer({ class: 'main' }, [
+  div({ class: 'wrapper' }, [
+    'made with a few bits of ',
+    Link({ href: 'https://github.com/magic/core', target: '_blank', rel: 'noopener' }, 'magic'),
+  ]),
+])
+
+Footer.style: {
+  'footer.main': {
+    position: 'relative',
+    textAlign: 'center',
+    padding: '5em 0 .5em',
   },
-  View: () =>
-    footer({ class: 'main' }, [
-      div({ class: 'wrapper' }, [
-        'made with a few bits of ',
-        Link({ href: 'https://github.com/magic/core', target: '_blank', rel: 'noopener' }, 'magic'),
-      ]),
-    ]),
 }
 
-// /assets/index.js
-module.exports = {
-  // ...other assets
-  Footer: require('./Footer'),
-}
+module.exports = Footer
 `),
 
     ModuleList,
