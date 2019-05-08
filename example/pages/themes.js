3484c99f2876c4ebb77650e4eb1aab43bf026563
Jascha <jascha@jaeh.at>
2019-05-09 00:57:02 +0200

    nicer theme inheritance list

--- example/pages/themes.js
+++ example/pages/themes.js
@@ -19,9 +19,12 @@ module.exports = {
 
     h2('theme load order'),
     p('themes get loaded from multiple places. last in the list overwrites earlier entries.'),
-    p('/node_modules/@magic/core/src/themes/${config.THEME}/index.js'),
-    p('/node_modules/@magic-themes/${config.THEME}'),
-    p('/assets/themes/${config.THEME}/index.js'),
+    Pre(`
+/node_modules/@magic/core/src/themes/THEME/index.js
+/node_modules/@magic-themes/THEME
+// ...default module styles get inserted here
+/assets/themes/THEME/index.js
+`),
 
     h2({ id: 'magic-themes' }, 'list of magic themes'),
     ThemeList,
