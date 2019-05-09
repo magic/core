768ce858d2bda3402e4052c47a2caffdcdc7aa9d
Jascha <jascha@jaeh.at>
2019-05-09 21:25:48 +0200

    format

--- example/pages/index.js
+++ example/pages/index.js
@@ -20,9 +20,9 @@ module.exports = {
         ' @magic makes it possible to create stunningly fast webpages.',
       ]),
 
-      h2({ id: 'privacy'}, 'privacy'),
+      h2({ id: 'privacy' }, 'privacy'),
       p([
-        '@magic does not spy on it\'s users.',
+        "@magic does not spy on it's users.",
         ' we not only try to be legally compliant,',
         ' but additionally to be ethical and do the right thing for your users.',
       ]),
@@ -34,7 +34,8 @@ module.exports = {
         '~4 kb javascript boilerplate.',
         ' usually, all the javascript in your homepage will be 30-60kb big (after unpacking),',
         ' 10-30kb get transmitted from the server to the client.',
-        ' this complete documentation page you are reading with all sub pages has about 50kb unpacked, 15kb transmitted. (values may be out of date)',
+        ' this complete documentation page you are reading with all sub pages loads about 50kb of unpacked,',
+        ' 15kb of gzipped javascript. (values may be out of date and depend on cdn settings)',
       ]),
 
       h3('@magic works without javascript'),
@@ -44,10 +45,7 @@ module.exports = {
       ]),
 
       h3('@magic generates static pages'),
-      p([
-        'this makes free hosting (using github or gitlab pages) possible.',
-        " and it's easy.",
-      ]),
+      p(['this makes free hosting (using github or gitlab pages) possible.', " and it's easy."]),
 
       p([
         '@magic publishes to ',
