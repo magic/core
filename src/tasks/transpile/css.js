256467a0eee511f38b12d6a5bb44731f785f33c7
Jascha <jascha@jaeh.at>
2019-05-10 18:53:08 +0200

    tasks/transpile/css: add THEME_VARS to css calls

--- src/tasks/transpile/css.js
+++ src/tasks/transpile/css.js
@@ -1,8 +1,8 @@
 const css = require('@magic/css')
 
 module.exports = async ([reset, style]) => {
-  reset = await css(reset)
-  style = await css(style)
+  reset = await css(reset, config.THEME_VARS)
+  style = await css(style, config.THEME_VARS)
 
   return {
     css: `/* reset */\n${reset.css}\n/* app */\n${style.css}`,
