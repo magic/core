0a991f51bb1cecf523de65f5ecc46e3a288adac8
Jascha <jascha@jaeh.at>
2019-05-10 23:04:04 +0200

    app.hashes is sorted alphabetically

--- src/tasks/transpile/index.js
+++ src/tasks/transpile/index.js
@@ -11,20 +11,29 @@ const transpile = async app => {
   const bundle = await client(app.client)
   const css = await style(app.style)
 
-  app.hashes = {
-    pages: {},
-    ...app.hashes,
-    js: createFileHash(bundle),
+  const hashes = {
     css: createFileHash(config.ENV === 'production' ? css.minified : css.css),
+    js: createFileHash(bundle),
   }
 
   const pages = html(app)
 
-  pages.forEach(page => {
-    page.hash = createFileHash(page.rendered)
-    // app.hashes.pages[`${page.name}`] = page.hash
-    app.hashes.pages[page.name] = page.hash
+  hashes.pages = {}
+
+  pages
+    .sort((a, b) => a.name < b.name ? -1 : 1)
+    .forEach(page => {
+      page.hash = createFileHash(page.rendered)
+      // app.hashes.pages[`${page.name}`] = page.hash
+      hashes.pages[page.name] = page.hash
+    })
+
+  hashes.static = {}
+  Object.entries(app.hashes.static).sort(([a], [b]) => a > b ? 1 : -1).forEach(([name, val]) => {
+    hashes.static[name] = val
   })
+
+  app.hashes = hashes
 
   app.static[`/${config.HASH_FILE_NAME}`] = JSON.stringify(app.hashes, null, 2)
 
