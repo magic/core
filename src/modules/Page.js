ac7a74c0a181633e977ae2885c344a6f5f448d8c
Jascha <jascha@jaeh.at>
2019-05-10 18:53:48 +0200

    format

--- src/modules/Page.js
+++ src/modules/Page.js
@@ -1,14 +1,12 @@
-module.exports = page => (state, actions) => div(
-  {
-    class: 'Wrapper',
-    oncreate: () => {
-      if (typeof window !== 'undefined' && actions.go) {
-        window.addEventListener('popstate', actions.go)
-      }
+module.exports = page => (state, actions) =>
+  div(
+    {
+      class: 'Wrapper',
+      oncreate: () => {
+        if (typeof window !== 'undefined' && actions.go) {
+          window.addEventListener('popstate', actions.go)
+        }
+      },
     },
-  },
-  [
-    Header,
-    div({ class: 'Page' }, page ? page(state, actions) : '404 - not found'),
-    Footer,
-  ])
+    [Header, div({ class: 'Page' }, page ? page(state, actions) : '404 - not found'), Footer],
+  )
