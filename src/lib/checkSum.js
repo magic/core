00cd5b5700237f6b35deacb15e9bcee1cc59fbc8
Jascha <jascha@jaeh.at>
2019-05-11 18:19:18 +0200

    remove unused file

--- src/lib/checkSum.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto')
-
-const calcHash = str => {
-  const hash = crypto.createHash('sha384')
-  hash.update(str)
-  const value = hash.digest('base64')
-  return `sha384-${value}`
-}
