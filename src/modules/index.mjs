f308201cfb696e99f26dd3ae2e65bdd0e0f649c3
Jascha <jascha@jaeh.at>
2019-05-19 05:51:06 +0200

    corecctly import Router, Page and Link

--- src/modules/index.mjs
+++ src/modules/index.mjs
@@ -1,8 +1,6 @@
-import { Page } from './Page.mjs'
-import * as Menu from './Menu.mjs'
-import * as Link from './Link.mjs'
+import * as Route from './Router.mjs'
+import { Menu } from './Menu.mjs'
 import * as Footer from './Footer.mjs'
-import * as Magic from './admin/index.mjs'
 import { Header } from './Header.mjs'
 import { Img } from './Img.mjs'
 
@@ -10,14 +8,16 @@ import { tags } from './tags.mjs'
 
 export { component } from './component.mjs'
 
+const { Link, Page, ...Router } = Route
+
 export const builtins = {
-  Page,
   Menu,
-  Link,
   Footer,
-  Magic,
   Header,
   Img,
+  Page,
+  Link,
+  Router,
   ...tags,
 }
 
