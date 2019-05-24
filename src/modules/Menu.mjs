0e7683b0d490304882e2b6fef7778e01a8722862
Jascha <jascha@jaeh.at>
2019-05-24 21:05:04 +0200

    modules/Menu: fix link wrapping

--- src/modules/Menu.mjs
+++ src/modules/Menu.mjs
@@ -1,6 +1,6 @@
 export const Menu = p => {
   CHECK_PROPS(p, propTypes, 'Menu')
-  let { items, hash, url = '', root, class: cl = 'Menu', collapse = true, ...props } = p
+  let { items, hash, url = '', root, class: cl = 'Menu', collapse = true } = p
 
   if (!items.length) {
     return
@@ -10,7 +10,7 @@ export const Menu = p => {
     url += `#${hash}`
   }
 
-  return nav({ class: cl }, ul(items.map(i => Menu.Item({ ...i, url, root, collapse }))))
+  return nav({ class: cl }, ul(items.map(i => Menu.Item({ ...i, root, url, collapse }))))
 }
 
 Menu.style = {
@@ -31,7 +31,6 @@ export const propTypes = Menu.propTypes = {
     { key: 'items', type: 'array', required: true },
     { key: 'hash', type: 'string' },
     { key: 'url', type: 'string' },
-    { key: 'root', type: 'string' },
     { key: 'collapse', type: 'boolean' },
     { key: 'class', type: 'string' },
   ],
@@ -41,14 +40,13 @@ export const propTypes = Menu.propTypes = {
     { key: 'items', type: 'array' },
     { key: 'parentTo', type: 'string' },
     { key: 'collapse', type: 'boolean' },
-    { key: 'root', type: 'string' },
     { key: 'to', type: 'string' },
   ]
 }
 
 Menu.Item = props => {
   CHECK_PROPS(props, propTypes, 'MenuItem')
-  const { url, text, items = [], parentTo = undefined, collapse, root, ...item } = props
+  const { url, text, items = [], root, parentTo = undefined, collapse, ...item } = props
   // if the item has no values, we quit
   if (!item.to && !text) {
     return
@@ -58,28 +56,28 @@ Menu.Item = props => {
     class: 'MenuItem',
   }
 
+
   if (parentTo) {
-    const isExternal = item.to.includes('://')
-    const isAbsolute = item.to.startsWith('/')
-    const startsLikeParent = !parentTo || item.to.startsWith(parentTo)
-    if (!startsLikeParent && !isAbsolute && !isExternal) {
-      if (!parentTo.endsWith('/') && !item.to.startsWith('-')) {
-        parentTo = `${parentTo}/`
+    const first = item.to[0]
+    const isLocal = first === '#' || first === '/' || first === '-'
+    const startsLikeParent = item.to.startsWith(parentTo)
+    if (!startsLikeParent && isLocal) {
+      if (parentTo.endsWith('/') && item.to.endsWith('/')) {
+        item.to = item.to.substr(1)
       }
 
       item.to = parentTo + item.to
     }
   }
 
-  item.to = item.to.startsWith(root) ? item.to : `${root}${item.to.substr(1)}`
-  const active = url && url.startsWith(item.to)
-  if (url === item.to) {
+  const active = url && url.includes(item.to)
+  if (url.endsWith(item.to)) {
     p.class += ' active'
   }
 
   let children = []
   if ((items.length && active) || !collapse) {
-    children = ul(items.map(i => Menu.Item({ parentTo: item.to, url, root, collapse, ...i })))
+    children = ul(items.map(i => Menu.Item({ parentTo: item.to, url, collapse, ...i })))
   }
 
   return li(p, [item.to ? Link(item, text) : span(item, text), children])
