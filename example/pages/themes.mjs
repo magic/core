56ff7152ae00972043770b10533c1f6ce8d6ce2d
Jascha <jascha@jaeh.at>
2019-05-24 21:05:37 +0200

    pages/themes: remove redundant header, change theme load order info

--- example/pages/themes.mjs
+++ example/pages/themes.mjs
@@ -19,12 +19,11 @@ export default {
   h2('theme load order'),
   p('themes get loaded from multiple places. last in the list overwrites earlier entries.'),
   Pre(`
+// ...default module styles get inserted here
 /node_modules/@magic/core/src/themes/THEME/index.js
 /node_modules/@magic-themes/THEME
-// ...default module styles get inserted here
 /assets/themes/THEME/index.js
 `),
 
-  h2({ id: 'magic-themes' }, 'list of magic themes'),
   ThemeList(),
 ]
