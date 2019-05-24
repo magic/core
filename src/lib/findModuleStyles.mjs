041458fd2bd9dca25787a58d7091bbf90bd91f78
Jascha <jascha@jaeh.at>
2019-05-24 21:10:33 +0200

    sort module styles alphabetically

--- src/lib/findModuleStyles.mjs
+++ src/lib/findModuleStyles.mjs
@@ -5,58 +5,63 @@ import { isUpperCase } from './isUpperCase.mjs'
 
 export const findModuleStyles = (modules, vars, parent) => {
   let styles = {}
-  Object.entries(modules).forEach(([name, mod]) => {
-    if (!is.empty(mod.style)) {
-      if (is.fn(mod.style)) {
-        mod.style = mod.style(vars)
-      }
+  Object.entries(modules)
+    .sort(([a], [b]) => a > b ? 1 : -1)
+    .forEach(([name, mod]) => {
+      if (!is.empty(mod.style)) {
+        if (is.fn(mod.style)) {
+          mod.style = mod.style(vars)
+        }
 
-      let selector = `.${name}`
-      let style = mod.style
+        let selector = `.${name}`
+        let style = mod.style
 
-      if (parent) {
-        selector = `.${parent}${name}`
-      }
+        if (parent) {
+          selector = `.${parent}${name}`
+        }
 
-      if (!mod.style[selector]) {
-        const modStyle = {}
-        const metaStyle = {}
-        Object.entries(mod.style).forEach(([k, v]) => {
-          if (k.startsWith('@')) {
-            if (k.startsWith('@media')) {
-              if (!Object.keys(v).some(kk => kk.includes(`.${k}`))) {
-                v = {
-                  [selector]: v,
+        if (!mod.style[selector]) {
+          const modStyle = {}
+          const metaStyle = {}
+          Object.entries(mod.style)
+            .sort(([a], [b]) => a > b ? 1 : -1)
+            .forEach(([k, v]) => {
+              if (k.startsWith('@')) {
+                if (k.startsWith('@media')) {
+                  if (!Object.keys(v).some(kk => kk.includes(`.${k}`))) {
+                    v = {
+                      [selector]: v,
+                    }
+                  }
                 }
+                metaStyle[k] = v
+              } else {
+                modStyle[k] = v
               }
-            }
-            metaStyle[k] = v
-          } else {
-            modStyle[k] = v
+            })
+
+          style = {}
+          if (!is.empty(modStyle)) {
+            style[selector] = modStyle
           }
-        })
-        style = {}
-        if (!is.empty(modStyle)) {
-          style[selector] = modStyle
+
+          style = {
+            ...style,
+            ...metaStyle,
+          }
         }
 
-        style = {
-          ...style,
-          ...metaStyle,
-        }
+        styles = deep.merge(styles, style)
       }
 
-      styles = deep.merge(styles, style)
-    }
-
-    Object.entries(mod)
-      .filter(([k]) => isUpperCase(k))
-      .filter(([_, m]) => !is.empty(m.style))
-      .forEach(([subName, subMod]) => {
-        const subStyles = findModuleStyles({ [subName]: subMod }, vars, name)
-        styles = deep.merge(styles, subStyles)
-      })
-  })
+      Object.entries(mod)
+        .filter(([k]) => isUpperCase(k))
+        .filter(([_, m]) => !is.empty(m.style))
+        .forEach(([subName, subMod]) => {
+          const subStyles = findModuleStyles({ [subName]: subMod }, vars, name)
+          styles = deep.merge(styles, subStyles)
+        })
+    })
 
   return styles
 }
