c0eadd0d27be6402119c29dd4f88efee227146df
Jascha <jascha@jaeh.at>
2019-05-27 20:44:41 +0200

    add app.state.root before transpiling pages

--- src/tasks/transpile/html.mjs
+++ src/tasks/transpile/html.mjs
@@ -1,17 +1,15 @@
 import { default as render } from 'hyperapp-render'
-import { h } from 'hyperapp/src/index.js'
 
 import log from '@magic/log'
 import deep from '@magic/deep'
 
-import { applyWebRoot } from '../../lib/index.mjs'
+export default (app, hashes) => {
+  app.state.root = config.WEB_ROOT
 
-export default (app, hashes) =>
-  app.pages.map(page => {
+  return app.pages.map(page => {
     try {
       app.state.url = page.name
       const state = deep.merge(page.state, app.state)
-      const actions = deep.merge(page.actions, app.actions)
 
       const view = app.View(page, hashes)
 
@@ -26,3 +24,4 @@ export default (app, hashes) =>
       process.exit(1)
     }
   })
+}
