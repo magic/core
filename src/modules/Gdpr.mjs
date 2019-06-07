08edeac01b175201b52f6b56b0b7d634441fba7a
Jascha <jascha@jaeh.at>
2019-06-07 22:26:49 +0200

    start handling cookies if they are set

--- src/modules/Gdpr.mjs
+++ src/modules/Gdpr.mjs
@@ -1,31 +1,92 @@
-export const View = gdpr => [
-  gdpr.show && div({ class: { Gdpr: true, show: gdpr.show } }, [
+export const View = ({ gdpr, cookies }) => {
+  if (!gdpr.show) {
+    return
+  }
+
+  cookies = Object.entries(cookies)
+
+  return div({ class: { Gdpr: true, show: gdpr.show } }, [
     input({ type: 'checkbox', name: 'show-hide', id: 'show-hide', checked: !gdpr.show }),
     div({ class: 'Container' }, [
       gdpr.title && h3(gdpr.title),
-      gdpr.content && div(gdpr.content),
-      !!gdpr.cookies.length && ul(gdpr.cookies.map(({ name, info }) => li([name, info]))),
-      label({ class: 'button', for: 'show-hide', onclick: actions.gdpr.close }, gdpr.buttonText),
+      gdpr.content && p(gdpr.content),
+      !!cookies.length
+        ? [
+            gdpr.cookieText && p(gdpr.cookieText),
+            ul(
+              cookies
+                .sort(([_, { required }]) => (required ? 0 : 1))
+                .map(([name, { info, allowed = false }]) =>
+                  li([
+                    input({
+                      type: 'checkbox',
+                      title: 'allow',
+                      checked: allowed,
+                      onchange: [actions.gdpr.allow, { name }],
+                    }),
+                    label([name, info && [' - ', info]]),
+                  ]),
+                ),
+            ),
+          ]
+        : p(gdpr.noCookieText),
+
+      cookies.length
+        ? [
+            label(
+              {
+                class: 'button',
+                for: 'show-hide',
+                onclick: [actions.gdpr.close, { allowed: true }],
+              },
+              gdpr.allowAllCookiesButtonText,
+            ),
+            label(
+              {
+                class: 'button',
+                for: 'show-hide',
+                onclick: actions.gdpr.close,
+              },
+              gdpr.allowCookieButtonText,
+            ),
+            label(
+              {
+                class: 'button',
+                for: 'show-hide',
+                onclick: [actions.gdpr.close, { allowed: false }],
+              },
+              gdpr.denyCookieButtonText,
+            ),
+          ]
+        : label(
+            { class: 'button', for: 'show-hide', onclick: actions.gdpr.close },
+            gdpr.noCookieButtonText,
+          ),
     ]),
-  ]),
-]
+  ])
+}
 
 export const state = {
   show: true,
-  cookies: false,
+  cookies: {},
   title: 'Magic Privacy Information',
-  content: 'This page does neither save, collect, nor share any personal data about you.',
-  buttonText: 'Awesome.',
+  noCookieText: 'This page does neither save, collect, nor share any personal data about you.',
+  noCookieButtonText: 'Awesome.',
+  cookieButtonText: 'Awesome.',
+  allowAllCookiesButtonText: 'Allow all',
+  allowCookieButtonText: 'Allow selected',
+  denyCookieButtonText: 'Deny all',
+  cookieText: 'We are using the following cookies on this page',
 }
 
 export const actions = {
   gdpr: {
     show: (state, p) => ({
-        ...state,
-        gdpr: {
-          ...state.gdpr,
-          ...p.value,
-        },
+      ...state,
+      gdpr: {
+        ...state.gdpr,
+        ...p.value,
+      },
     }),
 
     load: state => [
@@ -33,17 +94,38 @@ export const actions = {
       [effects.gdpr.readLocalStorage, { key: 'gdpr', action: actions.gdpr.show }],
     ],
 
-    close: state => [
-      {
-        ...state,
-        gdpr: {
-          ...state.gdpr,
-          cookies: [],
-          show: false,
+    close: (state, { allowed }) => {
+      const { cookies } = state
+      if (typeof allowed === 'boolean') {
+        Object.entries(state.cookies).forEach(([name, cookie]) => {
+          cookies[name] = {
+            ...cookie,
+            allowed,
+          }
+        })
+      }
+
+      return [
+        {
+          ...state,
+          gdpr: {
+            ...state.gdpr,
+            show: false,
+          },
+          cookies,
         },
-      },
-      [effects.gdpr.writeLocalStorage, { key: 'gdpr', value: { cookies: state.gdpr.cookies || [], show: false } }],
-    ],
+        [
+          effects.gdpr.writeLocalStorage,
+          { key: 'gdpr', value: { cookies: state.cookies || [], show: false } },
+        ],
+      ]
+    },
+    allow: (state, props) => {
+      state.cookies[props.name].allowed = true
+      return {
+        ...state,
+      }
+    },
   },
 }
 
@@ -76,13 +158,19 @@ export const style = (vars = {}) => ({
   left: 0,
   textAlign: 'center',
 
+  ul: {
+    display: 'block',
+  },
+
   '.Container': {
     backgroundColor: vars.colors.gray[900],
     border: '1px solid',
+    borderRadius: '.5em',
     display: 'inline-block',
     margin: '0 auto',
     padding: '1em',
     position: 'relative',
+    textAlign: 'left',
 
     '.light&&': {
       backgroundColor: vars.colors.gray[50],
@@ -90,7 +178,9 @@ export const style = (vars = {}) => ({
   },
 
   'input[type=checkbox]': {
-    display: 'none',
+    '&#show-hide': {
+      display: 'none',
+    },
 
     '&:checked ~ .Container': {
       opacity: 0,
@@ -118,3 +208,18 @@ export const style = (vars = {}) => ({
     },
   },
 })
+
+export const global = {
+  state: {
+    cookies: true,
+  },
+}
+
+// export const cookies = {
+//   'test-cookie-name-required': {
+//     info: 'required test cookie info',
+//     required: true,
+//     value: 'test-cookie-value',
+//   },
+//   'test-cookie-name': { info: 'test cookie info', value: 'test-cookie-value' },
+// }
