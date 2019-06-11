627573f4fb3861c316961f8d75d13c02afaf17a9
Jascha <jascha@jaeh.at>
2019-06-11 21:31:34 +0200

    update example, docs

--- example/pages/index.mjs
+++ example/pages/index.mjs
@@ -16,14 +16,15 @@ export const View = state => [
     p([
       '@magic aims to make it easy to stitch together any kind of webapp.',
       ' by providing simple, well documented and self contained modules,',
-      ' @magic makes it possible to create stunningly fast webpages.',
+      ' @magic makes it possible to create stunningly fast',
+      ' webpages with minimal cognitive overhead.',
     ]),
 
     h2({ id: 'privacy' }, 'privacy'),
     p([
       "@magic does not spy on it's users.",
       ' we not only try to be legally compliant,',
-      ' but additionally to be ethical and do the right thing for your users.',
+      ' but to be ethical and do the right thing for all users of the web.',
     ]),
 
     h2({ id: 'buzzwords' }, 'why should i use magic?'),
@@ -33,14 +34,15 @@ export const View = state => [
       '~4 kb javascript boilerplate.',
       ' usually, all the javascript in your homepage will be 30-60kb big (after unpacking),',
       ' 10-30kb get transmitted from the server to the client.',
-      ' this complete documentation page you are reading with all sub pages loads about 50kb of unpacked,',
-      ' 15kb of gzipped javascript. (values may be out of date and depend on cdn settings)',
+      ' this complete documentation page you are reading with all sub pages loads about 40kb of unpacked,',
+      ' 15kb of gzipped javascript.',
     ]),
 
     h3('@magic works without javascript'),
     p([
       'most of the functionality works without javascript,',
-      " buttons and realtime user interactions obviously won't.",
+      " some buttons and realtime user interactions obviously won't,",
+      ' but @magic always tries to provide a non-javascript fallback via css.'
     ]),
 
     h3('@magic generates static pages'),
@@ -60,7 +62,8 @@ export const View = state => [
       ' serverless lambdas, derived from the ',
       Link({ to: 'https://github.com/magic-modules/' }, '@magic-modules'),
       ' you use in your pages.',
-      ' this makes visitor statistics, user authentication and authorization, chat, and all other server side services possible.',
+      ' this makes visitor statistics, user authentication and authorization,',
+      ' chat, and all other server side services possible.',
     ]),
   ]),
 
