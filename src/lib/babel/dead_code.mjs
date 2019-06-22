4aa71b48bc7ea22fcd759184c02f2562c74dcab2
Jascha <jascha@jaeh.at>
2019-06-22 23:12:51 +0200

    continue babel rework

--- src/lib/babel/dead_code.mjs
+++ src/lib/babel/dead_code.mjs
@@ -11,38 +11,23 @@ const used = {
   tags: new Set(),
 }
 
-const handleEventFunctions = (path, t) => {
-  if (t.isArrayExpression(path.node.value)) {
-    const { elements } = path.node.value
-    const [action, helper] = elements
-    if (action.object && action.object.object) {
-      const actionObjectName = action.object.object.name
-      const actionPropertyName = action.object.property && action.object.property.name
-      const actionName = action.property.name
-      used[actionObjectName][actionPropertyName] = used[actionObjectName][actionPropertyName] || {}
-
-      used[actionObjectName][actionPropertyName][actionName] = {}
-    }
-    if (helper.object && helper.object.object) {
-      const helperObjectName = helper.object.object.name
-      const helperPropertyName = helper.object.property && helper.object.property.name
-      const helperName = helper.property.name
-      if (helperObjectName === 'helper') {
-        used.helper[helperPropertyName] = used.helper[helperPropertyName] || {}
-        used.helper[helperPropertyName][helperName] = {}
-      }
-    }
-  } else {
-    console.log(path.node)
-  }
-}
-
-const findInteractionsByType = (app, t, path, type) => {
+const handleLink = (path, config) => {
   const { node } = path
-  const { value } = node.key
+  const href = node.value.value
 
-  if (app[type].hasOwnProperty(value)) {
-    console.log({ value })
+  // console.log('href', href)
+  if (!href.startsWith(config.WEB_ROOT)) {
+    const isLocal = href.startsWith('/') && !href.startsWith('//')
+    const isHash = href.startsWith('#') || href.startsWith('/#')
+    const isExtension = href.startsWith('-')
+
+    // if (isExtension) {
+    //   console.log('is extension', href, path)
+    // }
+
+    if (isLocal || isHash) {
+      node.value.value = `${config.WEB_ROOT}${href}`.replace(/\/\//g, '/')
+    }
   }
 }
 
@@ -50,9 +35,10 @@ const findInteractionsByType = (app, t, path, type) => {
 const findUsedSpells = (t, app, config) => path => {
   // find used modules and html tags in app
   const moduleNames = Object.keys(app.modules)
+
   if (t.isCallExpression(path.node)) {
     if (t.isIdentifier(path.node.callee)) {
-      const name = path.node.callee.name
+      const { name } = path.node.callee
       const isModule = isModuleTag(name, moduleNames)
       if (isModule) {
         used.tags.add(name)
@@ -73,90 +59,92 @@ const findUsedSpells = (t, app, config) => path => {
         }
       }
     } else {
-      // console.log('unexpected callee type', path.node)
-    }
-  }
-
-  const handleLink = (node, config) => {
-    const href = node.value.value
-    if (href.startsWith('/') && !href.startsWith(config.WEB_ROOT)) {
-      node.value.value = `${config.WEB_ROOT}${href}`.replace(/\/\//g, '/')
+      // console.log('unexpected callee type', path.node.callee)
     }
   }
 
   if (t.isObjectProperty(path.node)) {
     if (path.node.key) {
+      const validKeys = ['src', 'logo'] //, 'href', 'to']
       if (path.node.key.name) {
         const { name } = path.node.key
         if (path.node.value.value) {
-          if (name === 'src' || name === 'logo') {
-            handleLink(path.node, config)
+          if (validKeys.includes(name)) {
+            handleLink(path, config)
           }
         }
       } else if (path.node.key.value) {
         const { value: name } = path.node.key
-        if (name === 'src' || name === 'logo') {
-          handleLink(path.node, config)
+        if (validKeys.includes(name)) {
+          handleLink(path, config)
         }
       }
-    } else {
-      console.log('no key in node', path.node)
     }
   } else if (t.isArrayExpression(path.node)) {
     const [action, helper] = path.node.elements
-    if (!action) {
-      return
-    }
 
-    if (t.isMemberExpression(action)) {
-      const { name: type } = action.object
-      if (used.hasOwnProperty(type)) {
-        if (t.isMemberExpression(action.object)) {
-          const { name: objName } = action.object.object
-          const { name: propName } = action.object.property
-          used[type][objName] = used[type][objName] || {}
-          used[type][objName][propName] = {}
-        } else if (t.isIdentifier(action.object)) {
-          const { name: objName } = action.object
-          const { name: propName } = action.property
-          used[type][objName] = used[type][objName] || {}
-          used[type][objName][propName] = {}
+    if (action) {
+      if (t.isMemberExpression(action)) {
+        const { name: type } = action.object
+        if (used[type]) {
+          if (t.isMemberExpression(action.object)) {
+            const { name: objName } = action.object.object
+            const { name: propName } = action.object.property
+            console.log({ objName, propName })
+            if (type !== objName) {
+              used[type][objName] = used[type][objName] || {}
+              used[type][objName][propName] = {}
+            } else {
+              used[type] = used[type] || {}
+              used[type][propName] = {}
+            }
+          } else if (t.isIdentifier(action.object)) {
+            const { name: objName } = action.object
+            const { name: propName } = action.property
+            if (objName !== type) {
+              used[type][objName] = used[type][objName] || {}
+              used[type][objName][propName] = {}
+            } else {
+              used[type] = used[type] || {}
+              used[type][propName] = {}
+            }
+          }
         }
       }
-    }
 
-    if (t.isMemberExpression(helper)) {
-      const { name: type } = helper.object
-      if (used.hasOwnProperty(type)) {
-        if (t.isMemberExpression(helper.object)) {
-          const { name: objName } = helper.object.object
-          const { name: propName } = helper.object.property
-          used[type][objName] = used[type][objName] || {}
-          used[type][objName][propName] = {}
-        } else if (t.isIdentifier(helper.object)) {
-          const { name: objName } = helper.object
-          const { name: propName } = helper.property
-          used[type][objName] = used[type][objName] || {}
-          used[type][objName][propName] = {}
+      if (t.isMemberExpression(helper)) {
+        const { name: type } = helper.object
+        if (used.hasOwnProperty(type)) {
+          if (t.isMemberExpression(helper.object)) {
+            const { name: objName } = helper.object.object
+            const { name: propName } = helper.object.property
+            used[type][objName] = used[type][objName] || {}
+            used[type][objName][propName] = {}
+          } else if (t.isIdentifier(helper.object)) {
+            const { name: objName } = helper.object
+            const { name: propName } = helper.property
+            used[type][objName] = used[type][objName] || {}
+            used[type][objName][propName] = {}
+          }
         }
       }
+      // } else if (t.isAssignmentExpression(path.node)) {
+      //   const { left, right } = path.node
+      //   let objName
+      //   let propName
+
+      //   if (t.isMemberExpression(left)) {
+      //     // props.onclick = action
+      //     // console.log('member expression', left)
+      //     objName = left.object.name
+      //     propName = left.property.name
+      //   } else if (t.isIdentifier(left)) {
+      //     // props = { onclick: action }
+      //     objName = left.name
+      //   }
+
+      //   console.log({ objName, propName })
     }
-    // } else if (t.isAssignmentExpression(path.node)) {
-    //   const { left, right } = path.node
-    //   let objName
-    //   let propName
-
-    //   if (t.isMemberExpression(left)) {
-    //     // props.onclick = action
-    //     // console.log('member expression', left)
-    //     objName = left.object.name
-    //     propName = left.property.name
-    //   } else if (t.isIdentifier(left)) {
-    //     // props = { onclick: action }
-    //     objName = left.name
-    //   }
-
-    //   console.log({ objName, propName })
   }
 }
 
@@ -197,6 +185,9 @@ const removeUnused = (t, app) => path => {
         // remove unused actions
         path.node.init.properties.forEach(prop => {
           const { value: name } = prop.key
+
+          // console.log({ actions: used.actions })
+
           if (t.isFunctionExpression(prop.value)) {
             if (!used.actions || !used.actions.hasOwnProperty(name)) {
               // console.log('remove fn', name)
@@ -206,7 +197,6 @@ const removeUnused = (t, app) => path => {
             }
           } else if (t.isObjectExpression(prop.value)) {
             const { value: propName } = prop.value.properties[0].key
-
             if (
               !used.actions ||
               !used.actions[name] ||
