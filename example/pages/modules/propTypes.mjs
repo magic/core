f4dc8e4ab1a3c68a4b5182f7cada6bee1c8ae160
Jascha <jascha@jaeh.at>
2019-08-05 15:46:27 +0200

    update example

--- example/pages/modules/propTypes.mjs
+++ example/pages/modules/propTypes.mjs
@@ -8,7 +8,28 @@ export const View = state => [
 
   h2({ id: 'check-props' }, 'CHECK_PROPS'),
   p('@magic-modules can export a .propTypes object with an array of prop types.'),
-  p('more docs coming soon'),
+
+  h4({ to: '#example' }, 'example'),
+  Pre(`
+export const View = (prop1, prop2, prop3) => [
+  p(prop1),
+  p(prop2),
+  p(prop3),
+]
+
+export const propTypes = [
+  { name: 'prop1', type: 'string' },
+  { name: 'prop2', type: 'number' },
+  { name: 'prop3', type: 'array', items: 'string' },
+  {
+    name: 'prop4',
+    type: 'object',
+    items: [
+      { name: 'prop4prop1', type: 'string' },
+      { name: 'prop4prop2', type: 'number' },
+  ] },
+]
+`),
 
   LightSwitch(state),
 ]
