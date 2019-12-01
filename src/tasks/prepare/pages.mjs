5d2fd022c4c50c79aab833df7419830a389afd85
Jascha <jascha@jaeh.at>
2019-12-01 23:22:05 +0100

    handle markdown and html pages

--- src/tasks/prepare/pages.mjs
+++ src/tasks/prepare/pages.mjs
@@ -1,9 +1,45 @@
 import is from '@magic/types'
 import path from 'path'
+import transmute from '@magic/transmute'
+import { fs, mkdirp, rmrf } from '../../lib/index.mjs'
+
+const tmpDir = path.join(process.cwd(), '.temp')
 
 export const preparePages = async files => {
   const pagePromises = files.map(async file => {
-    const pageTmp = await import(file)
+    const ext = path.extname(file)
+
+    const markdownExtensions = ['.md', '.markdown']
+    const htmlExtensions = ['.html', '.htm']
+
+    let transmuted = ''
+
+    if (markdownExtensions.includes(ext)) {
+      const content = await fs.readFile(file, 'utf8')
+      transmuted = transmute.markdown(content)
+      file = file
+        .replace('.md', '.mjs')
+        .replace('.markdown', '.mjs')
+    } else if (htmlExtensions.includes(ext)) {
+      const content = await fs.readFile(file, 'utf8')
+      transmuted = transmute.html(content)
+      file = file
+        .replace('.html', '.mjs')
+        .replace('.htm', '.mjs')
+    }
+
+
+    let pageTmp
+    if (transmuted) {
+      const viewString = `export const View = () => [${transmuted}]`
+      const fileTmpPath = path.join(tmpDir, path.basename(file))
+      await mkdirp(tmpDir)
+      await fs.writeFile(fileTmpPath, viewString)
+      pageTmp = await import(fileTmpPath)
+    } else {
+      pageTmp = await import(file)
+    }
+
     let page
     if (is.fn(pageTmp)) {
       page = {
@@ -15,6 +51,7 @@ export const preparePages = async files => {
     }
 
     page.file = file
+
     const pageName = file
       .replace(config.DIR.PAGES, '')
       .replace(/index.[m]?js/gm, '')
@@ -33,6 +70,7 @@ export const preparePages = async files => {
     page.path = path.join(config.DIR.PUBLIC, pageName)
     if (page.path.endsWith('/')) {
       if (page.name === '/404/') {
+        // page path for 404 is /404.html, not /404/index.html
         page.path = `${page.path.slice(0, -1)}.html`
       } else {
         page.path = path.join(page.path, 'index.html')
@@ -64,6 +102,11 @@ export const View = () => []
       View: () => div('404 - not found'),
     }
     pages.push(page404)
+  }
+
+  const tmpExists = await fs.exists(tmpDir)
+  if (tmpExists) {
+    await rmrf(tmpDir)
   }
 
   return pages
