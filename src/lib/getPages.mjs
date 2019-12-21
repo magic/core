12c61b5664e2aaa0ae63a282371bbbdb50d61911
Jascha <jascha@jaeh.at>
2019-12-21 05:11:06 +0100

    src/lib/getPages: throw error instead of logging it.

--- src/lib/getPages.mjs
+++ src/lib/getPages.mjs
@@ -9,16 +9,18 @@ export const getPages = async () => {
     if (files.length === 0) {
       throw new Error('no files')
     }
+
     return files
   } catch (e) {
     if (config.DIR.PAGES.startsWith(process.cwd())) {
-      log.warn('NOEXIST', `${config.DIR.PAGES} does not exist or does not contain pages`)
-      // const indexPage = "div('hello world')"
+      throw new Error(`NOEXIST - ${config.DIR.PAGES} does not exist or does not contain pages`)
+
+      // TODO: automatically install an example and give the user a choice which one
+      // const indexPage = "() => div('hello world')"
       // const pagePath = path.join(config.DIR.PAGES, 'index.mjs')
       // await fs.mkdirp(config.DIR.PAGES)
       // await fs.writeFile(pagePath, indexPage)
       //~ return await getPages()
-      process.exit()
     }
 
     throw e
