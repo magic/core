c9c0e75094fb62ae9c2eec2dac4eeb00c084e3d0
Jascha <jascha@jaeh.at>
2019-12-21 04:27:12 +0100

    add try-catch to build process promises

--- src/cluster/build.mjs
+++ src/cluster/build.mjs
@@ -1,3 +1,5 @@
+import log from '@magic/log'
+
 import runApp from '../modules/app.mjs'
 import runCmd from './runCmd.mjs'
 
@@ -7,14 +9,18 @@ export const build = async (cmds, config) => {
   const App = await runApp(config)
   const app = await runCmd('prepare', App, config)
 
-  const { bundle, css, pages /*, serviceWorker */ } = await runCmd('transpile', app, config)
-  app.pages = pages
-  app.css = css
-  app.client = bundle.code
-  // app.sw = serviceWorker.code
+  try {
+    const { bundle, css, pages /*, serviceWorker */ } = await runCmd('transpile', app, config)
+    app.pages = pages
+    app.css = css
+    app.client = bundle.code
+    // app.sw = serviceWorker.code
 
-  if (cmds.build) {
-    await runCmd('write', app, config)
+    if (cmds.build) {
+      await runCmd('write', app, config)
+    }
+  } catch(e) {
+    log.error('error during build', e)
   }
 
   if (cmds.serve) {
