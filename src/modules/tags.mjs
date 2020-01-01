01930c9d9010174bcc98ef8f52975cbe6a1fe49d
Jascha <jascha@jaeh.at>
2020-01-01 02:52:41 +0100

    export svg modules as tags

--- src/modules/tags.mjs
+++ src/modules/tags.mjs
@@ -4,15 +4,16 @@ import { bodyTags, headTags, svgTags } from '@magic/tags'
 
 const exp = {}
 
-bodyTags.forEach(name => {
+const prepareTag = name => {
   const prepared = component(name)
   exp[name] = prepared
-})
+}
 
-headTags.forEach(name => {
-  const prepared = component(name)
-  exp[name] = prepared
-})
+bodyTags.forEach(prepareTag)
+
+svgTags.forEach(prepareTag)
+
+headTags.forEach(prepareTag)
 
 const description = content => {
   exp.meta({ name: 'description', property: 'og:description', content })
