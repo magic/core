6bd45b2527156def1f2e82c3738b319e6ea19f45
Jascha <jascha@jaeh.at>
2020-01-16 19:28:37 +0100

    cluster/runCmd: use log.hrtime and log.timeTaken

--- src/cluster/runCmd.mjs
+++ src/cluster/runCmd.mjs
@@ -3,11 +3,12 @@ import log from '@magic/log'
 import * as tasks from '../tasks/index.mjs'
 
 export const runCmd = async (cmd, ...args) => {
-  const startTime = new Date().getTime()
+  const startTime = log.hrtime()
+
   const result = await tasks[cmd](...args)
-  const endTime = new Date().getTime()
-  const timeTaken = Math.ceil(endTime - startTime)
-  console.log(`${cmd}: ${timeTaken}ms`)
+
+  log.timeTaken(startTime, cmd)
+
   return result
 }
 
