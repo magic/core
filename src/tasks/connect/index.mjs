e362451bf884939baf9798d06542078188a70f43
Jascha <jascha@jaeh.at>
2020-01-16 19:50:02 +0100

    tasks/connect: use log.timeTaken and log.hrtime

--- src/tasks/connect/index.mjs
+++ src/tasks/connect/index.mjs
@@ -1,21 +1,21 @@
 import log from '@magic/log'
 
-// git subtree push --prefix example/public origin gh-pages
 import { getGitConfig, xc } from '../../lib/index.mjs'
 
 export const connect = async config => {
+  const startTime = log.hrtime()
+
   const git = await getGitConfig(config)
 
-  // const outDir = OUT_DIR.replace(`${process.cwd()}/`, '')
   const dir = config.DIR.PUBLIC.replace(`${process.cwd()}/`, '')
   const cmdPrefix = `--prefix=${dir}`
   const cmdOnto = `${git.ORIGIN} ${git.BRANCH}`
   const cmdArgv = `${cmdPrefix} ${cmdOnto}`
   const cmd = `git subtree push ${cmdArgv}`
 
-  log.time(cmd)
   await xc(cmd)
-  log.timeEnd(cmd)
+
+  log.timeTaken(startTime, cmd)
 }
 
 export default connect
