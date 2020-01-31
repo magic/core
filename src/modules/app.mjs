63b39986fb8786d31e501c34e0a83e83c4161b25
jascha <jascha@jaeh.at>
2020-01-31 20:54:00 +0100

    modules/App: add viewport and ie=edge meta tags

--- src/modules/app.mjs
+++ src/modules/app.mjs
@@ -46,8 +46,10 @@ const App = async config => {
         html({ lang: state.lang || LANG }, [
           head([
             meta({ charset: 'utf-8' }),
-            link(magicCss),
+            meta({ 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' }),
+            meta({ name: 'viewport', content: 'width=device-width, initial-scale=1.0' }),
             Seo(state),
+            link(magicCss),
             page.Head && page.Head(state),
           ]),
           body([
