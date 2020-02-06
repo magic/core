08a6716a4ab2031ab403ab9fc8554756127433b0
jascha <jascha@jaeh.at>
2020-02-06 08:49:55 +0100

    remove tests for toCamel and toPascal, they get tested in @magic/cases

--- test/lib/toCamel.mjs
+++
@@ -1,7 +0,0 @@
-import { toCamel } from '../../src/lib/index.mjs'
-
-export default [
-  { fn: toCamel('testing-string'), expect: 'testingString' },
-  { fn: toCamel('lowercase'), expect: 'lowercase' },
-  { fn: toCamel('alreadyCamel'), expect: 'alreadyCamel' },
-]
--- test/lib/toPascal.mjs
+++
@@ -1,7 +0,0 @@
-import { toPascal } from '../../src/lib/index.mjs'
-
-export default [
-  { fn: toPascal('testing-string'), expect: 'TestingString' },
-  { fn: toPascal('lowercase'), expect: 'Lowercase' },
-  { fn: toPascal('AlreadyPascal'), expect: 'AlreadyPascal' },
-]
