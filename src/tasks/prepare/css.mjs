59d0665ae6009e4ba1dc9915d852504f9af38aa4
jascha <jascha@jaeh.at>
2020-02-06 20:21:26 +0100

    prepare/css: get THEME_VARS before transpiling styles. make sure order of style inheritance is correct

--- src/tasks/prepare/css.mjs
+++ src/tasks/prepare/css.mjs
@@ -10,14 +10,15 @@ const url = new URL(import.meta.url)
 const dirName = path.dirname(url.pathname)
 
 export const prepareCss = async ({ app, modules }) => {
-  const styles = []
+  const resetStyles = []
+  const themeStyles = []
+  const pageStyles = []
+  const appStyles = []
 
   let { THEME = '', THEME_VARS = {} } = config
-  if (is.fn(THEME_VARS)) {
-    THEME_VARS = THEME_VARS({ colors })
-  }
 
   THEME_VARS.colors = colors
+
   if (is.string(THEME)) {
     THEME = [THEME]
   }
@@ -27,20 +28,14 @@ export const prepareCss = async ({ app, modules }) => {
     // merge default reset css into styles
     const libResetCssFile = path.join(dirName, '..', '..', 'themes', 'reset.css.mjs')
     let { reset } = await import(libResetCssFile)
-    if (is.function(reset)) {
-      reset = reset(THEME_VARS)
-    }
 
-    styles.push(reset)
+    resetStyles.push(reset)
 
     const themePromises = THEME.map(async theme_name => {
       // find reset css in theme dir if it exists
       const maybeResetCssFile = path.join(config.DIR.THEMES, theme_name, 'reset.css.mjs')
       const { default: maybeResetCssStyles } = await import(maybeResetCssFile)
-      if (is.function(maybeResetCssStyles)) {
-        maybeResetCssStyles = maybeResetCssStyles(THEME_VARS)
-      }
-      styles.push(maybeResetCssStyles)
+      resetStyles.push(maybeResetCssStyles)
     })
 
     await Promise.all(themePromises)
@@ -50,10 +45,6 @@ export const prepareCss = async ({ app, modules }) => {
     }
   }
 
-  // load all styles from all modules
-  const moduleStyles = findModuleStyles(modules, THEME_VARS)
-  styles.push(moduleStyles)
-
   // load user's chosen theme, if it is set and exists, and merge it over the styles
   if (THEME) {
     const themePromises = THEME.map(async theme_name => {
@@ -61,11 +52,11 @@ export const prepareCss = async ({ app, modules }) => {
       const libThemeFile = path.join(dirName, '..', '..', 'themes', theme_name, 'index.mjs')
 
       try {
-        let { default: theme } = await import(libThemeFile)
-        if (is.function(theme)) {
-          theme = theme(THEME_VARS)
+        let { default: theme, vars } = await import(libThemeFile)
+        if (!is.empty(vars)) {
+          THEME_VARS = { ...THEME_VARS, ...vars }
         }
-        styles.push(theme)
+        themeStyles.push(theme)
       } catch (e) {
         if (!e.code || !e.code.includes('MODULE_NOT_FOUND')) {
           throw error(e)
@@ -74,11 +65,12 @@ export const prepareCss = async ({ app, modules }) => {
 
       // look if it exists in node_modules/${theme_name}
       try {
-        let { default: theme } = await import(theme_name)
-        if (is.function(theme)) {
-          theme = theme(THEME_VARS)
+        let { default: theme, vars } = await import(theme_name)
+
+        if (!is.empty(vars)) {
+          THEME_VARS = { ...THEME_VARS, ...vars }
         }
-        styles.push(theme)
+        themeStyles.push(theme)
       } catch (e) {
         if (!e.code || !e.code.includes('MODULE_NOT_FOUND')) {
           throw error(e)
@@ -89,11 +81,11 @@ export const prepareCss = async ({ app, modules }) => {
       // look if it exists in node_modules/@magic-themes/${theme_name}
       try {
         const themePath = `@magic-themes/${theme_name}`
-        let { default: theme } = await import(themePath)
-        if (is.function(theme)) {
-          theme = theme(THEME_VARS)
+        let { default: theme, vars } = await import(themePath)
+        if (!is.empty(vars)) {
+          THEME_VARS = { ...THEME_VARS, ...vars }
         }
-        styles.push(theme)
+        themeStyles.push(theme)
       } catch (e) {
         if (!e.code || !e.code.includes('MODULE_NOT_FOUND')) {
           throw error(e)
@@ -104,11 +96,11 @@ export const prepareCss = async ({ app, modules }) => {
       // load user's custom theme, overwriting both preinstalled and node_modules themes
       try {
         const maybeThemeFile = path.join(config.DIR.THEMES, theme_name, 'index.mjs')
-        let { default: theme } = await import(maybeThemeFile)
-        if (is.function(theme)) {
-          theme = theme(THEME_VARS)
+        let { default: theme, vars } = await import(maybeThemeFile)
+        if (!is.empty(vars)) {
+          THEME_VARS = { ...THEME_VARS, ...vars }
         }
-        styles.push(theme)
+        themeStyles.push(theme)
       } catch (e) {
         if (!e.code || !e.code.includes('MODULE_NOT_FOUND')) {
           throw error(e)
@@ -122,22 +114,22 @@ export const prepareCss = async ({ app, modules }) => {
   app.pages
     .filter(p => p.style)
     .forEach(page => {
-      if (is.function(page.style)) {
-        page.style = page.style(THEME_VARS)
+      if (!is.empty(page.styleVars)) {
+        THEME_VARS = { ...THEME_VARS, ...page.styleVars }
       }
-      styles.push(page.style)
+      pageStyles.push(page.style)
     })
 
   const maybeAppFile = path.join(config.ROOT, 'app.mjs')
   const fileName = new URL(import.meta.url).pathname
   if (maybeAppFile !== fileName) {
     try {
-      const { style } = await import(maybeAppFile)
+      const { style, styleVars } = await import(maybeAppFile)
       if (style) {
-        if (is.function(style)) {
-          style = style(THEME_VARS)
+        if (!is.empty(styleVars)) {
+          THEME_VARS = { ...THEME_VARS, ...styleVars }
         }
-        styles.push(style)
+        appStyles.push(style)
       }
     } catch (e) {
       if (!e.code || !e.code.includes('MODULE_NOT_FOUND')) {
@@ -146,5 +138,16 @@ export const prepareCss = async ({ app, modules }) => {
     }
   }
 
-  return styles
+  // load all styles from all modules
+  const moduleStyles = findModuleStyles(modules, THEME_VARS)
+
+  const styles = [
+    ...resetStyles,
+    moduleStyles,
+    ...themeStyles,
+    ...pageStyles,
+    ...appStyles,
+  ]
+
+  return styles.map(style => is.fn(style) ? style(THEME_VARS) : style)
 }
