457aa9a790ec960aa1a51bc22c1ea3cb0f101821
jascha <jascha@jaeh.at>
2020-02-16 05:13:18 +0100

    rename logo to img, var missing

--- src/modules/Logo.mjs
+++ src/modules/Logo.mjs
@@ -1,2 +1,2 @@
 export const View = ({ img, text, ...state }) =>
-  Link({ to: state.root, class: 'Logo' }, [logo && Img(img), text && span(text)])
+  Link({ to: state.root, class: 'Logo' }, [img && Img(img), text && span(text)])
