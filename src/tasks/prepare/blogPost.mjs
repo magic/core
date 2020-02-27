a4871887b66b4430146ade079420c8336479c1bd
jascha <jascha@jaeh.at>
2020-02-28 00:09:27 +0100

    tasks/prepare/blogPost: info message uses state => [] instead of () => []

--- src/tasks/prepare/blogPost.mjs
+++ src/tasks/prepare/blogPost.mjs
@@ -101,9 +101,9 @@ export const prepareBlogPost = ({ WEB_ROOT, PAGES }) => async file => {
       `
 ${page}
 needs to either
-export default () => []
+export default state => []
 or
-export const View = () => []
+export const View = state => []
 `,
       'E_INVALID_BLOGPOST',
     )
