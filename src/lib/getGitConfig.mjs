3cad8a679036cd45d3862fdfef0068871479618d
jascha <jascha@jaeh.at>
2020-05-03 15:49:58 +0200

    format

--- src/lib/getGitConfig.mjs
+++ src/lib/getGitConfig.mjs
@@ -15,11 +15,7 @@ export const getGitConfig = async (config = {}) => {
 
   const data = await xc('git remote -v')
   if (data.stdout) {
-    git.ORIGIN = data.stdout
-      .split('\n')[1]
-      .split('\t')[1]
-      .replace('(push)', '')
-      .trim()
+    git.ORIGIN = data.stdout.split('\n')[1].split('\t')[1].replace('(push)', '').trim()
 
     git.BRANCH = git.BRANCH || 'gh-pages'
     return git
