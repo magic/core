6c4f5b5add326762e2332107001fdfb08236d10e
jascha <jascha@jaeh.at>
2021-01-17 04:03:06 +0100

    tasks/prepare/themes: load theme from node_modules if magic is npm i -g

--- src/tasks/prepare/themes.mjs
+++ src/tasks/prepare/themes.mjs
@@ -61,6 +61,8 @@ export const prepareThemes = async ({ app, modules }) => {
           `@magic-themes/${theme_name}`,
           // see if it is installed locally.
           path.join(config.DIR.THEMES, theme_name, 'index.mjs'),
+          // if called from global magic, load with absolute path
+          path.join(config.NODE_MODULES, '@magic-themes', theme_name, 'src', 'index.mjs'),
         ]
 
         return await Promise.all(
