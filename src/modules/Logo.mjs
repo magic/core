047786a5afe3a83f3c4acfcb800825a3b346adb7
jaeh <jascha@jaeh.at>
2021-02-17 16:44:50 +0100

    Logo: add CHECK_PROPS

--- src/modules/Logo.mjs
+++ src/modules/Logo.mjs
@@ -1,2 +1,10 @@
-export const View = ({ logo, logotext, ...state }) =>
-  Link({ to: state.root, class: 'Logo' }, [logo && Img(logo), logotext && span(logotext)])
+export const View = ({ logo, logotext, root }) =>
+  CHECK_PROPS(props, propTypes, 'Logo') && Link({ to: root, class: 'Logo' }, [logo && Img(logo), logotext && span(logotext)])
+
+export const propTypes = {
+  Logo: [
+    { key: 'logo', type: 'string' },
+    { key: 'logotext', type: 'string' },
+    { key: 'root', type: 'string', reqired: true },
+  ],
+}
