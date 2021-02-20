d4b46192b35636f59cd989cc4f78f1b2c7d8f1d2
jaeh <jascha@jaeh.at>
2021-02-20 21:32:18 +0100

    tasks/clean: error message: link to issues

--- src/tasks/clean/index.mjs
+++ src/tasks/clean/index.mjs
@@ -15,7 +15,7 @@ export const clean = async config => {
       // do not delete above/outside the cwd
       const msg = `TRIED DELETING OUTSIDE OF CWD!
       directory: ${dir} is not within ${process.cwd()}
-      This is not good, please file an issue on https://github.com/magic/core or send me a mail: bug@jaeh.at`
+      This is not good, please file an issue on https://github.com/magic/core/issues/ or send me a mail: bug@jaeh.at`
       throw error(msg, 'E_DEL_OUTSIDE_CWD')
     }
 
