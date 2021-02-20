4ec7d5b93bb824634de55c05d71b1ac98957244c
jaeh <jascha@jaeh.at>
2021-02-20 19:27:00 +0100

    logo: make propType check get correct props

--- src/modules/Logo.mjs
+++ src/modules/Logo.mjs
@@ -1,5 +1,5 @@
 export const View = ({ logo, logotext, root }) =>
-  CHECK_PROPS(props, propTypes, 'Logo') &&
+  CHECK_PROPS({ logo, logotext, root }, propTypes, 'Logo') &&
   Link({ to: root, class: 'Logo' }, [logo && Img(logo), logotext && span(logotext)])
 
 export const propTypes = {
