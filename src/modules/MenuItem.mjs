79fb0943e402b71bbd720a2e9da12f5ef6058ed6
jaeh <jascha@jaeh.at>
2021-02-20 15:58:49 +0100

    modules/MenuItem: thanks to tasks/prepare/stateLinks, MenuItem does not need to know its parent

--- src/modules/MenuItem.mjs
+++ src/modules/MenuItem.mjs
@@ -1,7 +1,7 @@
 export const View = props => {
   CHECK_PROPS(props, propTypes, 'MenuItem', true)
 
-  const { text, items = [], url, parentTo = undefined, collapse, ...item } = props
+  const { collapse, items = [], text, url, ...item } = props
 
   const p = {
     class: {},
@@ -9,26 +9,18 @@ export const View = props => {
 
   let { to } = item
 
-  const first = item.to[0]
-
-  if (parentTo && (first === '-' || first === '#')) {
-    to = parentTo + to
-  }
-
-  item.to = to.replace(/\/\//g, '/')
-
-  if (item.to === url) {
+  if (to === url) {
     p.class.active = true
   }
 
   let children = []
-  const active = url.startsWith(item.to) || !collapse
+  const active = !collapse || url.includes(to)
 
   if (active && items.length) {
-    children = ul(items.map(i => MenuItem({ parentTo: item.to, url, collapse, ...i })))
+    children = ul(items.map(i => MenuItem({ url, collapse, ...i })))
   }
 
-  return li(p, [item.to ? MenuLink(item, text) : span(item, text), children])
+  return li(p, [to ? Link(item, text) : span(item, text), children])
 }
 
 export const propTypes = {
@@ -46,7 +38,6 @@ export const propTypes = {
         ],
       },
     },
-    { key: 'parentTo', type: 'string' },
     { key: 'collapse', type: 'boolean' },
     { key: 'to', type: 'string' },
   ],
