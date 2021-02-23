c67f0b9c105f93c3f4257488d873fe4997b6b63c
jaeh <jascha@jaeh.at>
2021-02-23 17:13:50 +0100

    prepare/globals/findThemes: expect config to be passed as arg, change module handling to avoid mutable object

--- src/tasks/prepare/globals/findThemes.mjs
+++ src/tasks/prepare/globals/findThemes.mjs
@@ -5,13 +5,16 @@ import deep from '@magic/deep'
 import error from '@magic/error'
 import is from '@magic/types'
 
-export const findThemes = async (modules = {}) => {
-  let { THEME } = config
+export const findThemes = async conf => {
+  const { DIR, NODE_MODULES } = conf
+  let { THEME } = conf
 
   if (THEME) {
     if (is.string(THEME)) {
       THEME = [THEME]
     }
+
+    let modules = {}
 
     const url = new URL(import.meta.url)
     const dirName = path.dirname(url.pathname)
@@ -30,47 +33,53 @@ export const findThemes = async (modules = {}) => {
           `@magic-themes/${theme_name}`,
           // see if it is installed locally,
           // either in a subdirectory
-          path.join(config.DIR.THEMES, theme_name, 'index.mjs'),
+          path.join(DIR.THEMES, theme_name, 'index.mjs'),
           // or as a single mjs file
-          path.join(config.DIR.THEMES, theme_name + '.mjs'),
+          path.join(DIR.THEMES, theme_name + '.mjs'),
           // or from a globally installed magic
-          path.join(config.NODE_MODULES, '@magic-themes', theme_name, 'src', 'index.mjs'),
+          path.join(NODE_MODULES, '@magic-themes', theme_name, 'src', 'index.mjs'),
         ]
 
-        const modules = {}
-
-        await Promise.all(
+        const modules = await Promise.all(
           themeLocations.map(async location => {
             try {
               const { default: theme, ...maybeModules } = await import(location)
 
-              Object.entries(maybeModules).map(([name, fn]) => {
-                if (is.fn(fn)) {
-                  if (!modules[name]) {
-                    modules[name] = fn
-                  } else if (modules[name].View) {
-                    modules[name] = {
-                      ...modules[name],
-                      View: fn,
-                    }
-                  } else {
-                    modules[name] = fn
-                  }
-                } else if (is.object(fn)) {
-                  if (name[0].toUpperCase() === name[0].toUpperCase()) {
+              return Object.fromEntries(
+                Object.entries(maybeModules).map(([name, fn]) => {
+                  if (is.fn(fn)) {
                     if (!modules[name]) {
-                      modules[name] = { ...fn }
-                    } else if (is.fn(modules[name])) {
-                      modules[name] = { View: modules[name], ...fn }
+                      return [name, fn]
+                    } else if (modules[name].View) {
+                      return [
+                        name,
+                        {
+                          ...modules[name],
+                          View: fn,
+                        },
+                      ]
                     } else {
-                      modules[name] = {
-                        ...modules[name],
-                        ...fn,
+                      return [name, fn]
+                    }
+                  } else if (is.object(fn)) {
+                    if (name[0].toUpperCase() === name[0].toUpperCase()) {
+                      if (!modules[name]) {
+                        return [name, fn]
+                      } else if (is.fn(modules[name])) {
+                        return [name, { View: modules[name], ...fn }]
+                      } else {
+                        return [
+                          name,
+                          {
+                            ...modules[name],
+                            ...fn,
+                          },
+                        ]
                       }
                     }
                   }
-                }
-              })
+                }),
+              )
             } catch (e) {
               if (!e.code || !e.code.includes('MODULE_NOT_FOUND')) {
                 throw error(e)
@@ -79,7 +88,7 @@ export const findThemes = async (modules = {}) => {
           }),
         )
 
-        return modules
+        return modules.filter(a => a)
       }),
     )
 
@@ -92,7 +101,7 @@ export const findThemes = async (modules = {}) => {
         modules = deep.merge(modules, result)
       }
     })
-  }
 
-  return modules
+    return deep.flatten(modules)
+  }
 }
