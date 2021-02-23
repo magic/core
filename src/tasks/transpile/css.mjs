a28518fe7fa5f718e6b8afd6ca1ee112a28cba89
jaeh <jascha@jaeh.at>
2021-02-23 19:24:12 +0100

    transpile/css: just export css from @magic/css

--- src/tasks/transpile/css.mjs
+++ src/tasks/transpile/css.mjs
@@ -1,8 +1,3 @@
 import css from '@magic/css'
 
-export default async (style, vars) => {
-  // reset = await css(reset, config.THEME_VARS)
-  style = await css(style, vars)
-
-  return style
-}
+export default css
