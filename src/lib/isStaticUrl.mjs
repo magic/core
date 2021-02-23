c5f0d9ceb43b51f5d35f50510511ca56256b721a
jaeh <jascha@jaeh.at>
2021-02-23 19:26:53 +0100

    lib/isStaticUrl: use root argument instead of config.WEB_ROOT

--- src/lib/isStaticUrl.mjs
+++ src/lib/isStaticUrl.mjs
@@ -1,6 +1,6 @@
-export const isStaticUrl = (list, link) => {
-  if (link.startsWith(config.WEB_ROOT)) {
-    link = link.replace(config.WEB_ROOT, '/')
+export const isStaticUrl = (list, link, root) => {
+  if (link.startsWith(root)) {
+    link = link.replace(root, '/')
   }
 
   return list.includes(link)
