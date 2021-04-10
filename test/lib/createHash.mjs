6783e1bffadb82d64f6f5c696a550153eb76a519
jaeh <jascha@jaeh.at>
2021-04-10 17:42:47 +0200

    test: rename createFileHash to createHash

--- test/lib/createHash.mjs
+++ test/lib/createHash.mjs
@@ -1,9 +1,9 @@
 import { is } from '@magic/test'
 
-import { createFileHash } from '../../src/lib/index.mjs'
+import { createHash } from '../../src/lib/index.mjs'
 
 export default [
-  { fn: createFileHash('testing-string'), expect: is.string },
-  { fn: createFileHash('testing-string'), expect: t => t.startsWith('sha384-') },
-  { fn: createFileHash('testing-string'), expect: createFileHash('testing-string') },
+  { fn: createHash('testing-string'), expect: is.string },
+  { fn: createHash('testing-string'), expect: t => t.startsWith('sha384-') },
+  { fn: createHash('testing-string'), expect: createHash('testing-string') },
 ]
