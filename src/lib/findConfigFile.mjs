341f0b0fa29f02db8d377b2f45d403167f861d79
jaeh <jascha@jaeh.at>
2021-04-14 15:49:55 +0200

    lib/findConfigFile: format, add better log message if config file does not exist

--- src/lib/findConfigFile.mjs
+++ src/lib/findConfigFile.mjs
@@ -2,7 +2,6 @@ import path from 'path'
 
 import fs from '@magic/fs'
 import log from '@magic/log'
-import error from '@magic/error'
 
 import { replacePathSepForImport } from './replacePathSepForImport.mjs'
 
@@ -11,22 +10,25 @@ export const findConfigFile = async (ROOT, magicConfigNames, oldConfigName) => {
 
   let configFound = false
 
-  await Promise.all(magicConfigNames.map(async name => {
-    const fd = path.join(ROOT, name)
-    const exists = await fs.exists(fd)
+  await Promise.all(
+    magicConfigNames.map(async name => {
+      const fd = path.join(ROOT, name)
+      const exists = await fs.exists(fd)
 
-    if (exists) {
-      if (configFound) {
-        const msg = 'Found multiple config files. Make sure only one of the following files exist:\n'
-        const names = magicConfigNames.map(name => path.join(ROOT, name)).join('\n')
+      if (exists) {
+        if (configFound) {
+          const msg =
+            'Found multiple config files. Make sure only one of the following files exist:\n'
+          const names = magicConfigNames.map(name => path.join(ROOT, name)).join('\n')
 
-        log.warn('W_MULTI_CONFIG', msg, names, '\n')
+          log.warn('W_MULTI_CONFIG', msg, names, '\n')
+        }
+
+        configFound = true
+        filePath = replacePathSepForImport(fd)
       }
-
-      configFound = true
-      filePath = replacePathSepForImport(fd)
-    }
-  }))
+    }),
+  )
 
   if (!filePath) {
     const oldConfigFd = path.join(ROOT, oldConfigName)
@@ -35,13 +37,35 @@ export const findConfigFile = async (ROOT, magicConfigNames, oldConfigName) => {
     if (oldConfigExists) {
       filePath = replacePathSepForImport(oldConfigFd)
 
-      log.warn('W_OLD_CONFIG', 'You are using config.mjs, which is the old config. please rename the file to magic.js')
+      log.warn(
+        'W_OLD_CONFIG',
+        'You are using config.mjs, which is the old config. please rename the file to magic.js',
+      )
     }
   }
 
-
   if (!filePath) {
-    log.error('E_NO_CONFIG', 'No config file found. Please create magic.js (https://magic.github.io/core/files/#config)')
+    log.error(
+      'E_NO_CONFIG',
+      `No config file found. Please create ${ROOT}/magic.js (https://magic.github.io/core/files/#config)`,
+    )
+    log(`
+Example Config:
+
+export default {
+  ROOT: 'src', ${log.paint('green', '// the directory your pages, modules and themes live in')}
+  PUBLIC: 'docs', ${log.paint(
+    'green',
+    '// the output directory, docs gets accepted by github and gitlab pages',
+  )}
+  THEME: 'example', ${log.paint(
+    'green',
+    '// the @magic-theme you want to use. needs to be installed.',
+  )}
+  URL: 'https://example.com', ${log.paint('green', '// the root url of the site')}
+  WEB_ROOT: '/', ${log.paint('green', '// gets appended to the URL, should start and end with a /')}
+}`)
+
     process.exit()
   }
 
