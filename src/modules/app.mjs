41c98da7403c7bf5fec39cccd889560842dbe3c7
jaeh <jascha@jaeh.at>
2021-04-17 14:25:24 +0200

    modules/app: add Permissions-Policy: interest-cohort=() to avoid floc tracking

--- src/modules/app.mjs
+++ src/modules/app.mjs
@@ -208,6 +208,7 @@ const App = async config => {
             meta({ charset: 'utf-8' }),
             meta({ 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' }),
             meta({ name: 'viewport', content: 'width=device-width, initial-scale=1.0' }),
+            meta({ 'http-equiv': 'Permissions-Policy', content: 'interest-cohort=()' }),
             Seo({ ...state, seo }),
             link(magicCss),
             page.Head && page.Head(state),
