020dcf6ba289bc07f9a4625969b84608d42b4663
jascha <j@jaeh.at>
2021-12-04 15:09:08 +0100

    example: format

--- example/assets/static/appendJs.js
+++ example/assets/static/appendJs.js
@@ -1,1 +1,3 @@
-console.log('config.APPEND_JS works and appends after the magic.js. CONSOLE.LOG GETS REMOVED IN PROD, making this script not do anything.')
+console.log(
+  'config.APPEND_JS works and appends after the magic.js. CONSOLE.LOG GETS REMOVED IN PROD, making this script not do anything.',
+)
