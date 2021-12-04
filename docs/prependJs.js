909fab08502df7fbfc50317a7ddbef86eb61d5f1
jascha <j@jaeh.at>
2021-12-04 15:09:53 +0100

    docs: update

--- docs/prependJs.js
+++ docs/prependJs.js
@@ -1,1 +1,3 @@
-console.log('config.PREPEND_JS works and appends before the magic.js. CONSOLE.LOG GETS REMOVED IN PROD, making this script not do anything.')
+console.log(
+  'config.PREPEND_JS works and appends before the magic.js. CONSOLE.LOG GETS REMOVED IN PROD, making this script not do anything.',
+)
