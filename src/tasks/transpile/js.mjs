ffe9ecc4deeeebff49ac456cb53f792898f8729e
jaeh <jascha@jaeh.at>
2022-09-29 02:58:28 +0200

    tasks/transpile/js: remove babel code

--- src/tasks/transpile/js.mjs
+++ src/tasks/transpile/js.mjs
@@ -1,6 +1,6 @@
 import swc from '@swc/core'
 
-import { /* getBabelConf, */ getSwcConf } from '../../lib/index.mjs'
+import { getSwcConf } from '../../lib/index.mjs'
 
 export default async ({ app, config }) => {
   const swcOpts = getSwcConf(app, config)
@@ -12,36 +12,3 @@ export default async ({ app, config }) => {
     // serviceWorker: sw.code,
   }
 }
-
-// const babelOpts = getBabelConf(app, config)
-
-// const { code } = await babel.transform(app.client, babelOpts)
-// // const sw = await babel.transform(app.serviceWorker, babelOpts)
-
-// const magicPath = path.join(process.cwd(), '.tmp', '__magic__.js')
-// await fs.writeFile(magicPath, app.client)
-// console.log('wrote file', magicPath)
-
-// const opts = swcConfig({
-//   entry: {
-//     web: magicPath,
-//   },
-
-//   output: {
-//     path: path.join(process.cwd(), 'docs'),
-//     name: 'magic.js',
-//   },
-
-//   options: {
-//     minify: true,
-//     jsc: {
-//       minify: {
-//         compress: true,
-//         mangle: true,
-//       },
-//     },
-//   },
-// })
-
-// const bundle = await swc.bundle(opts)
-// console.log(bundle)
