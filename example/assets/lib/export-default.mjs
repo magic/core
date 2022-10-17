e4d20254d791b7b786c8447a8c8a9f798f176804
jaeh <jascha@jaeh.at>
2022-10-17 20:56:47 +0200

    example/assets/lib/export-default: do not wrap returned value in b()

--- example/assets/lib/export-default.mjs
+++ example/assets/lib/export-default.mjs
@@ -1,2 +1,2 @@
 const string = 'this string gets exported using export default,'
-export default a => b(`${string} the argument it received was ${a}`)
+export default a => `${string} the argument it received was ${a}`
