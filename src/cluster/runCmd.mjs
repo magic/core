fc366d4068a5d37324d8b3bd7a94ed33e16edeab
jaeh <jascha@jaeh.at>
2023-03-27 22:34:04 +0200

    cluster/runCmd: use trycatch

--- src/cluster/runCmd.mjs
+++ src/cluster/runCmd.mjs
@@ -5,11 +5,14 @@ import * as tasks from '../tasks/index.mjs'
 export const runCmd = async (cmd, ...args) => {
   const startTime = log.hrtime()
 
-  const result = await tasks[cmd](...args)
-
-  log.timeTaken(startTime, cmd)
-
-  return result
+  try {
+    const result = await tasks[cmd](...args)
+    log.timeTaken(startTime, cmd)
+    return result
+  } catch (e) {
+    log.timeTaken(startTime, cmd)
+    return e
+  }
 }
 
 export default runCmd
