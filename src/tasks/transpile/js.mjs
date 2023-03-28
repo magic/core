145ec1b8706e2886ab3958bbd292ab77efcb81bf
jaeh <jascha@jaeh.at>
2023-03-28 22:02:49 +0200

    tasks/transpile/js: add try/catch to log.error any swc errors instead of swallowing them

--- src/tasks/transpile/js.mjs
+++ src/tasks/transpile/js.mjs
@@ -1,14 +1,20 @@
+import log from '@magic/log'
+
 import swc from '@swc/core'
 
 import { getSwcConf } from '../../lib/index.mjs'
 
 export default async ({ app, config }) => {
   const swcOpts = getSwcConf(app, config)
-  const { code } = await swc.transform(app.client, swcOpts)
+  try {
+    const { code } = await swc.transform(app.client, swcOpts)
 
-  // const sw = await swc.transform(app.serviceWorker, swcOpts)
-  return {
-    code,
-    // serviceWorker: sw.code,
+    // const sw = await swc.transform(app.serviceWorker, swcOpts)
+    return {
+      code,
+      // serviceWorker: sw.code,
+    }
+  } catch (e) {
+    log.error('E_SWC', e)
   }
 }
